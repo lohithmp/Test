85d63d136466fd68cffca078f55875bd122be921
lohithmp <109272620+lohithmp@users.noreply.github.com>
2024-12-10 13:42:03 +0530

    Update test.java

--- test.java
+++ test.java
@@ -1,5 +1,11 @@
-    public String generateAccessToken(AccessTokenRequest accessTokenRequest) {
-        logger.debug("Initiate generateAccessToken for {}", accessTokenRequest);
-        AuthRequestValidator.validAuthRequest(accessTokenRequest);
-        return jwtService.generateAccessToken(accessTokenRequest);
-    }
+Argument passed to when() is null!
+Example of correct stubbing:
+    doThrow(new RuntimeException()).when(mock).someMethod();
+Also, if you use @Mock annotation don't miss openMocks()
+org.mockito.exceptions.misusing.NullInsteadOfMockException: 
+Argument passed to when() is null!
+Example of correct stubbing:
+    doThrow(new RuntimeException()).when(mock).someMethod();
+Also, if you use @Mock annotation don't miss openMocks()
+	at com.sbi.epay.authentication.service.AuthenticationServiceTest.testGenerationAccessToken(AuthenticationServiceTest.java:36)
+	at java.base/java.lang.reflect.Method.invoke(Method.java:580)
