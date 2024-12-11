14f7594f0e5d4f05d313ba48e2c3f98e10f82410
lohithmp <109272620+lohithmp@users.noreply.github.com>
2024-12-11 16:07:58 +0530

    Update test2.java

--- test2.java
+++ test2.java
@@ -42,3 +42,14 @@ public class CaptchaServiceTest {
         verify(captchaDao, times(1)).saveCaptcha(any(CaptchaDto.class));
     }
 }
+
+Cannot invoke "com.epay.merchant.dto.CaptchaDto.getRequestId()" because "captchaDto" is null
+java.lang.NullPointerException: Cannot invoke "com.epay.merchant.dto.CaptchaDto.getRequestId()" because "captchaDto" is null
+	at com.epay.merchant.service.CaptchaService.generateCaptcha(CaptchaService.java:50)
+	at com.epay.merchant.service.CaptchaServiceTest.generateCaptchaSuccess(CaptchaServiceTest.java:70)
+	at java.base/java.lang.reflect.Method.invoke(Method.java:580)
+	at java.base/java.util.ArrayList.forEach(ArrayList.java:1596)
+	at java.base/java.util.ArrayList.forEach(ArrayList.java:1596)
+
+
+OpenJDK 64-Bit Server VM warning: Sharing is only supported for boot loader classes because bootstrap classpath has been appended
