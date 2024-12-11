167cdbe63586bd899e82573617601020ae75eff2
lohithmp <109272620+lohithmp@users.noreply.github.com>
2024-12-11 17:19:12 +0530

    Update test2.java

--- test2.java
+++ test2.java
@@ -1,25 +1,39 @@
-@Test
-void getCaptchaFailureGenerateCaptchaText() throws NoSuchFieldException, IllegalAccessException {
-    // Set CAPTCHA_VALIDITY using reflection
-    Field field = CaptchaService.class.getDeclaredField("CAPTCHA_VALIDITY");
-    field.setAccessible(true);
-    field.set(captchaService, 60000L); // 60 seconds validity
 
-    // Create a spy for the CaptchaService
-    CaptchaService spyCaptchaService = spy(captchaService);
+@RequiredArgsConstructor
+@Service
+public class OtpService {
 
-    // Mock the generateCaptchaText method to throw an exception
-    doThrow(new RuntimeException("Failed to generate CAPTCHA text")).when(spyCaptchaService).generateCaptchaText();
+    private static final LoggerUtility log = LoggerFactoryUtility.getLogger(OtpController.class);
+    private final OtpDao otpDao;
 
-    String requestType = "requestType";
+    public MerchantResponse<OtpResponse> generateOTP(String userName) {
+        log.info(" generating OTP for User :: "+userName);
+        OtpDto otpDto = generaterandomOtp(userName);
+        log.info(" saving otp in database ");
+        otpDto = otpDao.saveOtp(otpDto);
+        log.info(" otp saved Successfully ");
+        return MerchantResponse.<OtpResponse>builder()
+               .data(List.of(OtpResponse.builder()
+                       .requestId(UUID.randomUUID())
+                       .otpDescription("OTP successfully generated for user: "+userName)
+                       .otp(otpDto.getOtp())
+                       .build()))
+               .status(1).build();
+    }
 
-    Exception exception = assertThrows(RuntimeException.class, () -> {
-        spyCaptchaService.getCaptcha(requestType);
-    });
+    private OtpDto generaterandomOtp(String userName) {
+        log.info(" Generating Random Otp ");
+        // Generate OTP (random number between 100000 and 999999)
+        int otp = new Random().nextInt(900000) + 100000;
+        // Set validity (5 minutes from now)
+        LocalDateTime validity = LocalDateTime.now().plusMinutes(5);
+       return  OtpDto.builder()
+               .userName(userName)
+               .otp(otp)
+               .status("G")
+               .validity(validity)
+               .createdAt(LocalDateTime.now())
+               .build();
+    }
 
-    assertEquals("Failed to generate CAPTCHA text", exception.getMessage());
-
-    // Verify that generateCaptchaText was called
-    verify(spyCaptchaService, times(1)).generateCaptchaText();
-    verify(spyCaptchaService, never()).generateCaptchaImage(anyString());
 }
