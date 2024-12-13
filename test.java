49e81a1d255da5144f5bc68140374443dc6c11bf
lohithmp <109272620+lohithmp@users.noreply.github.com>
2024-12-13 19:48:47 +0530

    Update test.java

--- test.java
+++ test.java
@@ -1,137 +1,46 @@
-package com.epay.merchant.service;
+ID	RAW	No		1	
+MERCHANT_ID	VARCHAR2(255 BYTE)	Yes		2	
+ORDER_REF_NUM	VARCHAR2(255 BYTE)	Yes		3	
+SBI_ORDER_REF_NUM	VARCHAR2(255 BYTE)	Yes		4	
+PAY_MODE	VARCHAR2(50 BYTE)	Yes		5	
+PAYMENT_MODE_DETAILS	CLOB	Yes		6	
+DEBIT_AMT	NUMBER(15,2)	Yes		7	
+CHANNEL_BANK	VARCHAR2(255 BYTE)	Yes		8	
+CIN	VARCHAR2(255 BYTE)	Yes		9	
+PUSH_RESPONSE	CLOB	Yes		10	
+TXN_REQUEST_COUNT	NUMBER(10,0)	Yes		11	
+FAIL_REASON	VARCHAR2(255 BYTE)	Yes		12	
+ATRN_NUM	VARCHAR2(255 BYTE)	Yes		13	
+STATUS	VARCHAR2(50 BYTE)	Yes		14	
+PAYMENT_STATUS	VARCHAR2(50 BYTE)	Yes		15	
+SETTLEMENT_STATUS	VARCHAR2(50 BYTE)	Yes		16	
+REFUND_STATUS	VARCHAR2(50 BYTE)	Yes		17	
+CANCELLATION_STATUS	VARCHAR2(50 BYTE)	Yes		18	
+CREATED_BY	VARCHAR2(255 BYTE)	Yes		19	
+UPDATED_BY	VARCHAR2(255 BYTE)	Yes		20	
+CREATED_DATE	NUMBER(13,0)	Yes		21	
+UPDATED_DATE	NUMBER(13,0)	Yes		22	
 
-import com.epay.merchant.dao.MerchantLoginDao;
-import com.epay.merchant.dto.MerchantUserDto;
-import com.epay.merchant.entity.MerchantUser;
-import com.epay.merchant.exceptions.MerchantException;
-import com.epay.merchant.model.request.AuthenticateUserRequest;
-import com.epay.merchant.model.response.MerchantResponse;
-import com.epay.merchant.util.ErrorConstants;
-import com.epay.merchant.validatior.MerchantLoginValidation;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.sbi.epay.authentication.service.AuthenticationService;
-import com.sbi.epay.authentication.model.UserTokenRequest;
-import com.sbi.epay.authentication.util.enums.TokenType;
-import com.sbi.epay.captcha.service.CaptchaService;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
 
-class MerchantLoginServiceTest {
 
-    @InjectMocks
-    private MerchantLoginService merchantLoginService;
+ORDERS
 
-    @Mock
-    private MerchantLoginDao merchantLoginDao;
-
-    @Mock
-    private MerchantLoginValidation merchantLoginValidation;
-
-    @Mock
-    private AuthenticationService authService;
-
-    @Mock
-    private CaptchaService captchaService;
-
-    @Mock
-    private ObjectMapper objectMapper;
-
-    private static final String VALID_USERNAME = "testUser";
-    private static final String VALID_PASSWORD = "password123";
-    private static final String VALID_TOKEN = "sampleToken";
-
-    private MerchantUserDto merchantUserDto;
-    private MerchantUser merchantUser;
-
-    @BeforeEach
-    void setUp() {
-        MockitoAnnotations.openMocks(this);
-
-        merchantUserDto = new MerchantUserDto();
-        merchantUserDto.setUserName(VALID_USERNAME);
-        merchantUserDto.setUserPassword(VALID_PASSWORD);
-        merchantUserDto.setRole("USER");
-        merchantUserDto.setMid("123456");
-
-        merchantUser = new MerchantUser();
-        merchantUser.setUserName(VALID_USERNAME);
-        merchantUser.setUserPassword(VALID_PASSWORD);
-    }
-
-    @Test
-    void loginUser_Success() {
-        // Mocking dependencies
-        when(merchantLoginValidation.validate(VALID_USERNAME)).thenReturn("USERNAME");
-        when(captchaService.isCaptchaValid(anyString(), anyString())).thenReturn(true);
-        when(merchantLoginDao.findUserByUsername(anyString(), anyString())).thenReturn(merchantUserDto);
-        when(objectMapper.convertValue(merchantUserDto, MerchantUser.class)).thenReturn(merchantUser);
-        when(authService.generateUserToken(any(UserTokenRequest.class))).thenReturn(VALID_TOKEN);
-
-        AuthenticateUserRequest userRequest = new AuthenticateUserRequest();
-        userRequest.setRequestId("12345");
-        userRequest.setCaptchaText("validCaptcha");
-        userRequest.setPassword(VALID_PASSWORD);
-
-        MerchantResponse<String> response = merchantLoginService.loginUser(VALID_USERNAME, userRequest);
-
-        // Assertions
-        assertNotNull(response);
-        assertEquals(1, response.getStatus());
-        assertEquals(VALID_TOKEN, response.getData().get(0));
-
-        // Verifying method calls
-        verify(merchantLoginValidation).validate(VALID_USERNAME);
-        verify(captchaService).isCaptchaValid(anyString(), anyString());
-        verify(merchantLoginDao).findUserByUsername(anyString(), anyString());
-        verify(authService).generateUserToken(any(UserTokenRequest.class));
-    }
-
-    @Test
-    void loginUser_InvalidCaptcha() {
-        when(captchaService.isCaptchaValid(anyString(), anyString())).thenReturn(false);
-
-        AuthenticateUserRequest userRequest = new AuthenticateUserRequest();
-        userRequest.setRequestId("12345");
-        userRequest.setCaptchaText("invalidCaptcha");
-        userRequest.setPassword(VALID_PASSWORD);
-
-        MerchantException exception = assertThrows(MerchantException.class, () ->
-                merchantLoginService.loginUser(VALID_USERNAME, userRequest)
-        );
-
-        assertEquals(ErrorConstants.ERROR_CAPTCHA_INVALID, exception.getCode());
-
-        verify(captchaService).isCaptchaValid(anyString(), anyString());
-        verifyNoInteractions(merchantLoginDao);
-        verifyNoInteractions(authService);
-    }
-
-    @Test
-    void loginUser_InvalidCredentials() {
-        when(merchantLoginValidation.validate(VALID_USERNAME)).thenReturn("USERNAME");
-        when(captchaService.isCaptchaValid(anyString(), anyString())).thenReturn(true);
-        when(merchantLoginDao.findUserByUsername(anyString(), anyString())).thenReturn(merchantUserDto);
-        when(objectMapper.convertValue(merchantUserDto, MerchantUser.class)).thenReturn(merchantUser);
-
-        AuthenticateUserRequest userRequest = new AuthenticateUserRequest();
-        userRequest.setRequestId("12345");
-        userRequest.setCaptchaText("validCaptcha");
-        userRequest.setPassword("wrongPassword");
-
-        MerchantException exception = assertThrows(MerchantException.class, () ->
-                merchantLoginService.loginUser(VALID_USERNAME, userRequest)
-        );
-
-        assertEquals(ErrorConstants.ERROR_INVALID_CREDENTIALS, exception.getCode());
-
-        verify(merchantLoginValidation).validate(VALID_USERNAME);
-        verify(captchaService).isCaptchaValid(anyString(), anyString());
-        verify(merchantLoginDao).findUserByUsername(anyString(), anyString());
-    }
-}
+ID	RAW	No	SYS_GUID() 	1	
+MERCHANT_ID	VARCHAR2(50 CHAR)	Yes		2	
+CUSTOMER_ID	VARCHAR2(50 CHAR)	Yes		3	
+CURRENCY_CODE	VARCHAR2(50 CHAR)	Yes		4	
+ORDER_AMOUNT	NUMBER	Yes		5	
+ORDER_REF_NUMBER	VARCHAR2(255 CHAR)	Yes		6	
+SBI_ORDER_REF_NUMBER	VARCHAR2(255 CHAR)	Yes		7	
+STATUS	VARCHAR2(50 CHAR)	Yes		8	
+OTHER_DETAILS	CLOB	Yes		9	
+EXPIRY	NUMBER	Yes		10	
+MULTI_ACCOUNTS	CLOB	Yes		11	
+PAYMENT_MODE	VARCHAR2(50 CHAR)	Yes		12	
+ORDER_HASH	VARCHAR2(500 CHAR)	Yes		13	
+CREATED_BY	VARCHAR2(50 CHAR)	Yes		14	
+UPDATED_BY	VARCHAR2(50 CHAR)	Yes		15	
+CREATED_DATE	NUMBER	Yes		16	
+UPDATED_DATE	NUMBER	Yes		17	
+RETURN_URL	VARCHAR2(500 BYTE)	Yes		18	
