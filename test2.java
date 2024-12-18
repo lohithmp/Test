c53dcb24f9c03dbcfef47b9597a290f68693973d
lohithmp <109272620+lohithmp@users.noreply.github.com>
2024-12-18 17:50:35 +0530

    Update test2.java

--- test2.java
+++ test2.java
@@ -1,26 +1,8 @@
- @Test
-    public void getPaymentStatusVerificationValidationFailure() {
-        try(MockedStatic<EPayIdentityUtil> epayMock = mockStatic(EPayIdentityUtil.class)) {
-            EPayPrincipal ePayPrincipal = new EPayPrincipal();
-            ePayPrincipal.setMid("123");
-
-            epayMock.when(EPayIdentityUtil::getUserPrincipal).thenReturn(ePayPrincipal);
-            when(orderDao.getActiveMerchantByMID(ePayPrincipal.getMid())).thenReturn(Optional.ofNullable(buildMerchantData()));
-
-            PaymentVerificationRequest request = new PaymentVerificationRequest();
-            request.setOrderRefNumber("");
-            request.setSbiOrderRefNumber("");
-            request.setAtrnNumber("");
-            request.setOrderAmount(new BigDecimal("100.0"));
-
-            List<ErrorDto> errorDtoList = new ArrayList<>();
-            errorDtoList.add(ErrorDto.builder().errorCode(ErrorConstants.MANDATORY_FOUND_ERROR_CODE).errorMessage(MessageFormat.format(ErrorConstants.MANDATORY_ERROR_MESSAGE, "Order Reference Number")).build());
-
-            doThrow(new ValidationException(errorDtoList)).when(validator).validatePaymentStatusRequest(request,buildOrder().getMID());
-
-            ValidationException expection = assertThrows(ValidationException.class,() -> {
-                paymentVerificationService.getPaymentStatusVerification(request);
-            });
-            assertEquals("Order Reference Number is mandatory.", expection.getErrorMessage());
+    private String encryptPaymentVerificationResponse(PaymentVerificationResponse paymentVerificationResponse, MerchantDto merchantDto) {
+        try {
+            return encryptionDecryptionUtil.encryptRequest(objectMapper.writeValueAsString(paymentVerificationResponse), merchantDto);
+        } catch (JsonProcessingException e) {
+            logger.error("Error in encryptPaymentVerificationData ", e);
+            throw new TransactionException(ErrorConstants.INVALID_ERROR_CODE, MessageFormat.format(ErrorConstants.INVALID_ERROR_MESSAGE, "paymentVerificationResponse","Error in encrypting pay"));
         }
     }
