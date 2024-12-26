2ea7516aa25266df584c8ff5f74b4e0c5476b407
lohithmp <109272620+lohithmp@users.noreply.github.com>
2024-12-26 19:24:55 +0530

    Update test2.java

--- test2.java
+++ test2.java
@@ -1,5 +1,5 @@
 Could not obtain required identifier from entity MerchantCacheEntity(merchantId=1, isActive=Active, merchantName=PineLabs, countryCode=INR1, currency=INR, preferredPayMode=UPI, preferredBank=SBI Bank)
-
+    
 curl --location 'http://localhost:9098/admin/v1/merchant/cacheMerchant/1' \
 --header 'Content-Type: application/json' \
 --data '{
@@ -17,25 +17,21 @@ curl --location 'http://localhost:9098/admin/v1/merchant/cacheMerchant/1' \
 
 
 '
+    public ResponseDto<String> saveMerchant(String mId, MerchantCacheEntity merchant) {
+        Optional<MerchantCacheEntity> optionalMerchant = merchantCacheRepository.findById(mId);
 
+        if (optionalMerchant.isPresent()) {
+            return ResponseDto.<String>builder().status(Status.FAIL.getValue()).total(0L).count(0L).data(List.of()).errors(List.of(ErrorDto.builder().errorCode(ErrorConstants.ALREADY_EXIST_ERROR_CODE).errorMessage(MessageFormat.format(ErrorConstants.ALREADY_EXIST_ERROR_MESSAGE, "Mid")).reason(MessageFormat.format(ErrorConstants.ALREADY_EXIST_ERROR_MESSAGE, "Mid")).build())).build();
+        }
+        try{
+            MerchantCacheEntity merchantCacheEntity =  merchantCacheRepository.save(merchant);
 
-package com.epay.admin.repository.cache;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
 
-import com.epay.admin.entity.cache.MerchantCacheEntity;
-import org.springframework.data.gemfire.repository.Query;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
-
-@Repository
-public interface MerchantCacheRepository extends CrudRepository<MerchantCacheEntity, String> {
-
-    @Query("SELECT m.merchantName, m.countryCode FROM /Merchant m WHERE m.merchantName =:merchantName")
-    Optional<MerchantCacheEntity> findMerchantNameAndCountry(String merchantName);
-}
-
-
+        return ResponseDto.<String>builder().status(Status.SUCCESS.getValue()).total(1L).count(1L).errors(List.of()).data(List.of("Merchant Data saved successfully.")).build();
+    }
 
 @Data
 @Region("Merchants")
@@ -61,21 +57,13 @@ public class MerchantCacheEntity {
 }
 
 
-package com.epay.admin.config;
+@Repository
+public interface MerchantCacheRepository extends CrudRepository<MerchantCacheEntity, String> {
 
-import com.epay.admin.entity.cache.MerchantCacheEntity;
-import com.epay.admin.repository.cache.MerchantCacheRepository;
-import org.apache.geode.cache.GemFireCache;
-import org.apache.geode.cache.client.ClientCache;
-import org.apache.geode.cache.client.ClientRegionShortcut;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.gemfire.client.ClientCacheFactoryBean;
-import org.springframework.data.gemfire.client.ClientRegionFactoryBean;
-import org.springframework.data.gemfire.config.annotation.ClientCacheApplication;
-import org.springframework.data.gemfire.config.annotation.EnableEntityDefinedRegions;
-import org.springframework.data.gemfire.repository.config.EnableGemfireRepositories;
-import org.springframework.data.gemfire.support.ConnectionEndpoint;
+    @Query("SELECT m.merchantName, m.countryCode FROM /Merchant m WHERE m.merchantName =:merchantName")
+    Optional<MerchantCacheEntity> findMerchantNameAndCountry(String merchantName);
+}
+
 
 @Configuration
 
@@ -84,11 +72,12 @@ import org.springframework.data.gemfire.support.ConnectionEndpoint;
 @EnableGemfireRepositories(basePackageClasses = MerchantCacheRepository.class)
 public class CacheConfig {
 
+
     @Bean
-    public ClientRegionFactoryBean<Long, MerchantCacheEntity> merchantRegion(GemFireCache gemFireCache) {
-        ClientRegionFactoryBean<Long, MerchantCacheEntity> merchantRegionFactory = new ClientRegionFactoryBean<>();
+    public ClientRegionFactoryBean<String, MerchantCacheEntity> merchantRegion(GemFireCache gemFireCache) {
+        ClientRegionFactoryBean<String, MerchantCacheEntity> merchantRegionFactory = new ClientRegionFactoryBean<>();
         merchantRegionFactory.setCache((ClientCache) gemFireCache);
-        merchantRegionFactory.setRegionName("merchants");
+        merchantRegionFactory.setRegionName("Merchants");
         merchantRegionFactory.setShortcut(ClientRegionShortcut.LOCAL);
         return merchantRegionFactory;
     }
