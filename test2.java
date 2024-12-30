df23f38a50315a14a7a41d69e8f96e6fe66f2b15
lohithmp <109272620+lohithmp@users.noreply.github.com>
2024-12-30 15:55:26 +0530

    Update test2.java

--- test2.java
+++ test2.java
@@ -1,84 +1,14 @@
-Could not obtain required identifier from entity MerchantCacheEntity(merchantId=1, isActive=Active, merchantName=PineLabs, countryCode=INR1, currency=INR, preferredPayMode=UPI, preferredBank=SBI Bank)
-    
-curl --location 'http://localhost:9098/admin/v1/merchant/cacheMerchant/1' \
---header 'Content-Type: application/json' \
---data '{
-    "merchantId": "1",
-    "isActive": "Active",
-    
-    
-    
-    "merchantName": "PineLabs",
-    "countryCode": "INR1",
-    "currency": "INR",
-    "preferredPayMode": "UPI",
-    "preferredBank": "SBI Bank"
-}
+ List<MerchantPaymode> merchantPaymodeList = new ArrayList<>();
+        if(merchantPayModeCacheList.isEmpty()) {
 
+            merchantPaymodeList = merchantPayModeDao.getMerchantPaymodeById(mId);
+            if (merchantPaymodeList.isEmpty()) {
+                return ResponseDto.<MerchantPaymodeDTO>builder().status(Status.FAIL.getValue()).total(0L).count(0L).data(List.of()).errors(List.of(ErrorDto.builder().errorCode(ErrorConstants.NOT_FOUND_ERROR_CODE).errorMessage(MessageFormat.format(ErrorConstants.NOT_FOUND_ERROR_MESSAGE, "Mid")).reason(MessageFormat.format(ErrorConstants.NOT_FOUND_ERROR_MESSAGE, "Mid")).build())).build();
+            }
 
-'
-    public ResponseDto<String> saveMerchant(String mId, MerchantCacheEntity merchant) {
-        Optional<MerchantCacheEntity> optionalMerchant = merchantCacheRepository.findById(mId);
+            List<MerchantPayModeCacheEntity> merchantCacheEntity = new ArrayList<MerchantPayModeCacheEntity>();
+            BeanUtils.copyProperties(merchantPaymodeList, merchantCacheEntity);
+//            merchantPaymodeList.stream().map(merchantPaymode -> ob);
+//            merchantCacheRepository.save(merchantCacheEntity);
 
-        if (optionalMerchant.isPresent()) {
-            return ResponseDto.<String>builder().status(Status.FAIL.getValue()).total(0L).count(0L).data(List.of()).errors(List.of(ErrorDto.builder().errorCode(ErrorConstants.ALREADY_EXIST_ERROR_CODE).errorMessage(MessageFormat.format(ErrorConstants.ALREADY_EXIST_ERROR_MESSAGE, "Mid")).reason(MessageFormat.format(ErrorConstants.ALREADY_EXIST_ERROR_MESSAGE, "Mid")).build())).build();
         }
-        try{
-            MerchantCacheEntity merchantCacheEntity =  merchantCacheRepository.save(merchant);
-
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-
-        return ResponseDto.<String>builder().status(Status.SUCCESS.getValue()).total(1L).count(1L).errors(List.of()).data(List.of("Merchant Data saved successfully.")).build();
-    }
-
-@Data
-@Region("Merchants")
-public class MerchantCacheEntity {
-
-    @Id
-    private String merchantId;
-
-    private String isActive;
-
-
-    private String merchantName;
-
-    private String countryCode;
-
-    private String currency;
-
-    private String preferredPayMode;
-
-    private String preferredBank;
-
-
-}
-
-
-@Repository
-public interface MerchantCacheRepository extends CrudRepository<MerchantCacheEntity, String> {
-
-    @Query("SELECT m.merchantName, m.countryCode FROM /Merchant m WHERE m.merchantName =:merchantName")
-    Optional<MerchantCacheEntity> findMerchantNameAndCountry(String merchantName);
-}
-
-
-@Configuration
-
-@ClientCacheApplication
-@EnableEntityDefinedRegions(basePackageClasses = MerchantCacheEntity.class ,clientRegionShortcut = ClientRegionShortcut.LOCAL)
-@EnableGemfireRepositories(basePackageClasses = MerchantCacheRepository.class)
-public class CacheConfig {
-
-
-    @Bean
-    public ClientRegionFactoryBean<String, MerchantCacheEntity> merchantRegion(GemFireCache gemFireCache) {
-        ClientRegionFactoryBean<String, MerchantCacheEntity> merchantRegionFactory = new ClientRegionFactoryBean<>();
-        merchantRegionFactory.setCache((ClientCache) gemFireCache);
-        merchantRegionFactory.setRegionName("Merchants");
-        merchantRegionFactory.setShortcut(ClientRegionShortcut.LOCAL);
-        return merchantRegionFactory;
-    }
-}
