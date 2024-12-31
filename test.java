ec3d330528744556550ca10fad30d22337a508a9
lohithmp <109272620+lohithmp@users.noreply.github.com>
2024-12-31 15:24:18 +0530

    Update test.java

--- test.java
+++ test.java
@@ -1,73 +1,73 @@
 private PaymentOptionsDTO getOtherPaymentOptions(List<MerchantPaymode> merchantPayModeList, Map<String, DownTimeDTO> downTimeDTOMap) {
-    // Create lists to hold different types of payment options
+    // Initializing the lists for different payment modes
     List<InbDTO> otherInbList = new ArrayList<>();
     List<CardDTO> otherCcCardList = new ArrayList<>();
     List<CardDTO> otherDcCardList = new ArrayList<>();
     List<CardDTO> otherPcCardList = new ArrayList<>();
 
-    // Helper method to add CardDTO to the respective list
-    Consumer<MerchantPaymode> addCardDTO = merchantPaymode -> {
-        CardDTO card = CardDTO.builder()
-            .bankName(merchantPaymode.getPayGatewayName())
-            .payproctype(merchantPaymode.getPayProc())
-            .aggregatorGatewayMapId(merchantPaymode.getAggregatorGatewayMapId())
-            .payproc(merchantPaymode.getPayProc())
-            .build();
-        switch (merchantPaymode.getPayModeCode().toUpperCase()) {
-            case ApplicationConstants.PAY_MODE_CODE_CREDITCARD -> otherCcCardList.add(card);
-            case ApplicationConstants.PAY_MODE_CODE_DEBITCARD -> otherDcCardList.add(card);
-            case ApplicationConstants.PAY_MODE_CODE_PREPAIDCARD -> otherPcCardList.add(card);
-        }
-    };
+    // Lambda to map payModeCode to its corresponding list
+    Map<String, List<CardDTO>> payModeToCardListMap = Map.of(
+            ApplicationConstants.PAY_MODE_CODE_CREDITCARD, otherCcCardList,
+            ApplicationConstants.PAY_MODE_CODE_DEBITCARD, otherDcCardList,
+            ApplicationConstants.PAY_MODE_CODE_PREPAIDCARD, otherPcCardList
+    );
 
-    // Process MerchantPaymode list
+    // Process merchantPayModeList
     for (MerchantPaymode merchantPaymode : merchantPayModeList) {
-        if (merchantPaymode.getPayModeCode().equalsIgnoreCase(ApplicationConstants.PAY_MODE_CODE_NETBANKING)) {
+        String payModeCode = merchantPaymode.getPayModeCode();
+
+        if (payModeCode.equalsIgnoreCase(ApplicationConstants.PAY_MODE_CODE_NETBANKING)) {
             otherInbList.add(InbDTO.builder()
-                .bankId(merchantPaymode.getPayGatewayId())
-                .bankName(merchantPaymode.getPayGatewayName())
-                .payproctype(merchantPaymode.getPayProc())
-                .payproc(merchantPaymode.getPayProc())
-                .aggregatorGatewayMapId(merchantPaymode.getAggregatorGatewayMapId())
-                .build());
+                    .bankId(merchantPaymode.getPayGatewayId())
+                    .bankName(merchantPaymode.getPayGatewayName())
+                    .payproctype(merchantPaymode.getPayProc())
+                    .payproc(merchantPaymode.getPayProc())
+                    .aggregatorGatewayMapId(merchantPaymode.getAggregatorGatewayMapId())
+                    .build());
         } else {
-            addCardDTO.accept(merchantPaymode);
+            List<CardDTO> cardList = payModeToCardListMap.get(payModeCode.toUpperCase());
+            if (cardList != null) {
+                cardList.add(CardDTO.builder()
+                        .bankName(merchantPaymode.getPayGatewayName())
+                        .payproctype(merchantPaymode.getPayProc())
+                        .aggregatorGatewayMapId(merchantPaymode.getAggregatorGatewayMapId())
+                        .payproc(merchantPaymode.getPayProc())
+                        .build());
+            }
         }
     }
 
-    // Process DownTimeDTO map
-    for (DownTimeDTO downtime : downTimeDTOMap.values()) {
-        CardDTO downtimeCard = CardDTO.builder().downtime(downtime).build();
-        switch (downtime.getPayModeCode().toUpperCase()) {
-            case ApplicationConstants.PAY_MODE_CODE_NETBANKING -> 
-                otherInbList.add(InbDTO.builder().downtime(downtime).build());
-            case ApplicationConstants.PAY_MODE_CODE_CREDITCARD -> 
-                otherCcCardList.add(downtimeCard);
-            case ApplicationConstants.PAY_MODE_CODE_DEBITCARD -> 
-                otherDcCardList.add(downtimeCard);
-            case ApplicationConstants.PAY_MODE_CODE_PREPAIDCARD -> 
-                otherPcCardList.add(downtimeCard);
+    // Process downTimeDTOMap
+    downTimeDTOMap.values().forEach(downTimeDTO -> {
+        String payModeCode = downTimeDTO.getPayModeCode();
+        if (payModeCode.equalsIgnoreCase(ApplicationConstants.PAY_MODE_CODE_NETBANKING)) {
+            otherInbList.add(InbDTO.builder().downtime(downTimeDTO).build());
+        } else {
+            List<CardDTO> cardList = payModeToCardListMap.get(payModeCode.toUpperCase());
+            if (cardList != null) {
+                cardList.add(CardDTO.builder().downtime(downTimeDTO).build());
+            }
         }
-    }
+    });
 
-    // Create CardsDTO
+    // Construct CardsDTO
     CardsDTO otherCardsDTO = CardsDTO.builder()
-        .cc(otherCcCardList)
-        .dc(otherDcCardList)
-        .pc(otherPcCardList)
-        .build();
+            .cc(otherCcCardList)
+            .dc(otherDcCardList)
+            .pc(otherPcCardList)
+            .build();
 
-    // Create UpiDTO
+    // Construct UpiDTO
     UpiDTO otherUpiDTO = UpiDTO.builder()
-        .apps(Arrays.asList(ApplicationConstants.PAY_MODE_DEFAULT_UPI_APPS))
-        .vpa(true)
-        .drawerIntent(true)
-        .build();
+            .apps(Arrays.asList(ApplicationConstants.PAY_MODE_DEFAULT_UPI_APPS))
+            .vpa(true)
+            .drawerIntent(true)
+            .build();
 
-    // Build and return PaymentOptionsDTO
+    // Construct and return PaymentOptionsDTO
     return PaymentOptionsDTO.builder()
-        .upi(otherUpiDTO)
-        .cards(otherCardsDTO)
-        .inb(otherInbList)
-        .build();
-}
+            .upi(otherUpiDTO)
+            .cards(otherCardsDTO)
+            .inb(otherInbList)
+            .build();
+        }
