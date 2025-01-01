de590e7e31fbb0e159503d1749ae45ed280f68d6
lohithmp <109272620+lohithmp@users.noreply.github.com>
2025-01-01 16:05:28 +0530

    Update test2.java

--- test2.java
+++ test2.java
@@ -1,119 +1,95 @@
-Caused by: org.springframework.beans.BeanInstantiationException: Failed to instantiate [org.springframework.data.gemfire.client.ClientRegionFactoryBean]: Factory method 'merchantRegion' threw exception with message: A connection to a distributed system already exists in this VM.  It has the following configuration:  ack-severe-alert-threshold="0"
+Caused by: org.springframework.core.convert.ConverterNotFoundException: No converter found capable of converting from type [org.apache.geode.pdx.internal.PdxInstanceImpl] to type [@org.springframework.data.gemfire.repository.Query com.epay.transaction.entity.cache.MerchantPayModeDownTimeCacheEntity]
+
+
+@Repository
+public interface DowntimeAPICacheRepository extends CrudRepository<MerchantPayModeDownTimeCacheEntity, String> {
+
+        @Query("SELECT payModeDownTime FROM /AGG_PayGateway_DownTime_DTLS payModeDownTime WHERE payModeDownTime.status = $1")
+        List<MerchantPayModeDownTimeCacheEntity> findByStatusAndRecordStatus(String status);
+
+}
+
+@Component
+@AllArgsConstructor
+public class DowntimeAPIDao {
+    private final LoggerUtility logger = LoggerFactoryUtility.getLogger(DowntimeAPIDao.class);
+    private final DowntimeAPICacheRepository downtimeAPICacheRepository;
+    private final AdminServicesClient adminServicesClient;
+
+    public List<DownTimeDTO> getDowntimeAPIs(String status) {
+        List<MerchantPayModeDownTimeCacheEntity> downtimeAPIs = downtimeAPICacheRepository.findByStatusAndRecordStatus(status);
+        List<DownTimeDTO> downTimeDTOList = new ArrayList<>();
+        for(MerchantPayModeDownTimeCacheEntity merchantPayModeDownTime : downtimeAPIs){
+            downTimeDTOList.add(DownTimeDTO.builder().gatewayId(merchantPayModeDownTime.getId()).startTimestamp(merchantPayModeDownTime.getStartTimestamp()).endTimestamp(merchantPayModeDownTime.getEndTimestamp()).status(merchantPayModeDownTime.getStatus()).payModeCode(merchantPayModeDownTime.getPayModeCode()).errorMessage(merchantPayModeDownTime.getErrorMessage()).build());
+        }
+        return downTimeDTOList;
+    }
+}
 
 
 
-package com.epay.admin.config;
+@Data
+@Region("AGG_PayGateway_DownTime_DTLS")
+public class MerchantPayModeDownTimeCacheEntity implements Serializable{
 
-import com.epay.admin.entity.cache.MerchantCacheEntity;
-import org.apache.geode.cache.GemFireCache;
-import org.apache.geode.cache.client.ClientCache;
-import org.apache.geode.cache.client.ClientCacheFactory;
-import org.apache.geode.cache.client.ClientRegionShortcut;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.gemfire.client.ClientCacheFactoryBean;
-import org.springframework.data.gemfire.client.ClientRegionFactoryBean;
-import org.springframework.data.gemfire.config.annotation.ClientCacheApplication;
-import org.springframework.data.gemfire.config.annotation.EnableEntityDefinedRegions;
-import org.springframework.data.gemfire.repository.config.EnableGemfireRepositories;
-import org.springframework.data.gemfire.support.ConnectionEndpoint;
+    @Id
+    @Column(name = "paygtwid")
+    private String id;
 
-import java.util.Collections;
+    @Column(name = "downtimestartdatetime")
+    private String startTimestamp;
+
+    @Column(name = "downtimeenddatetime")
+    private String endTimestamp;
+
+    @Column(name = "remarks")
+    private String errorMessage;
+
+    @Column(name = "status")
+    private String status;
+
+    @Column(name = "recordstatus")
+    private String recordStatus;
+
+    @Column(name = "paymodecode")
+    private String payModeCode;
+}
+
+
 
 @Configuration
 @ClientCacheApplication
-@EnableEntityDefinedRegions(basePackages = "com/epay/admin/entity/cache")
-@EnableGemfireRepositories(basePackages = "com/epay/admin/repository/cache")
-public class GemFireConfiguration  {
-
-    Logger logger = LoggerFactory.getLogger(GemFireConfiguration.class);
-
-    @Value("${cache.factory.PoolLocator}")
-    private String poolLocatorPath;
-
-    @Value("${cache.factory.port}")
-    private Integer poolLocatorPort;
-
-    @Value("${log.level}")
-    private String logLevel;
-
-    @Value("${ssl.enabled.components}")
-    private String sslEnabledComponents;
-
-    @Value("${ssl.keystore}")
-    private String sslKeystore;
-
-    @Value("${ssl.keystore.password}")
-    private String sslKeystorePassword;
-
-    @Value("${ssl.keystore.type}")
-    private String sslKeystoreType;
-
-    @Value("${ssl.truststore}")
-    private String sslTruststore;
-
-    @Value("${ssl.truststore.password}")
-    private String sslTruststorePassword;
-
-    @Value("${ssl.truststore.type}")
-    private String sslTruststoreType;
-
-    @Value("${ssl.endpoint.identification.enabled}")
-    private String sslEndpointIdentificationEnabled;
-
-    @Value("${ssl.require.authentication}")
-    private String sSLRequireAuthentication;
-
-    @Value("${ssl.web.require.authentication}")
-    private String sSLWebRequireAuthentication;
-
-    @Value("${ssl.protocols}")
-    private String sslProtocols;
-
-    @Value("${ssl.ciphers}")
-    private String sslCiphers;
-
-    /*@Value("${cache.region.name}")
-    private String cacheRegionName;*/
-
+@EnableEntityDefinedRegions(basePackages = "com/epay/transaction/entity/cache")
+@EnableGemfireRepositories(basePackages = "com/epay/transaction/repository/cache")
+public class GemFireConfiguration {
     public ClientCache clientCacheConfig() {
         return new ClientCacheFactory()
-                .addPoolLocator(poolLocatorPath, poolLocatorPort)
-                //.addPoolServer("gemfireserver.apps.dev.sbiepay.sbi",443)
-                .set("log-level", logLevel)
-                //.set("log-level","info")
-                // .set("ssl-endpoint-identification-enabled",sslEndpointIdentificationEnabled)
-                //.set("ssl-enabled-components",sslEnabledComponents)
-                .set("ssl-keystore", sslKeystore)
-                .set("ssl-keystore-password", sslKeystorePassword)
-                .set("ssl-keystore-type", sslTruststoreType)
-                .set("ssl-truststore", sslTruststore)
-                .set("ssl-truststore-password", sslTruststorePassword)
-                .set("ssl-truststore-type", sslTruststoreType)
-                .set("ssl-protocols", sslProtocols)
-                .set("ssl-ciphers", sslCiphers)
-                .set("ssl-require-authentication", sSLRequireAuthentication)
-                .set("ssl-web-require-authentication", sSLWebRequireAuthentication)
+                .addPoolLocator("SBIEPAYBD173.CORP.AD.SBI", 10334)
+                .addPoolServer("10.30.64.173", 40404)
                 .create();
     }
 
-//    @Bean
-//    public ClientCacheFactoryBean clientCache() {
-//        ClientCacheFactoryBean cacheFactoryBean = new ClientCacheFactoryBean();
-//
-//        cacheFactoryBean.setServers(Collections.singleton(new ConnectionEndpoint("localhost", 40404)));
-//        return cacheFactoryBean;
-//    }
 
     @Bean
-    public ClientRegionFactoryBean<String, MerchantCacheEntity> merchantRegion(GemFireCache gemFireCache) {
-       // ClientRegionFactoryBean<String, MerchantCacheEntity> merchantRegionFactory = new ClientRegionFactoryBean<>();
-        ClientRegionFactoryBean<String, MerchantCacheEntity> merchantRegionFactory = (ClientRegionFactoryBean<String, MerchantCacheEntity>) clientCacheConfig();
+    public ClientRegionFactoryBean<String, MerchantPayModeDownTimeCacheEntity> merchantRegion(GemFireCache gemFireCache) {
+        ClientRegionFactoryBean<String, MerchantPayModeDownTimeCacheEntity> merchantRegionFactory = new ClientRegionFactoryBean<>();
         merchantRegionFactory.setCache((ClientCache) gemFireCache);
         merchantRegionFactory.setShortcut(ClientRegionShortcut.PROXY);
         return merchantRegionFactory;
     }
 }
+
+
+APPLICATION.PROPERTIES
+
+spring.data.gemfire.locators=localhost[10334]
+spring.data.gemfire.cache.server.port=40404
+spring.data.gemfire.cache.region.static=true
+
+spring.data.gemfire.pdx.read-serialized=true
+spring.data.gemfire.pdx.auto-serializable-classes=C:/Users/V1014352/Epay/epay_transaction_service/src/main/java/com/epay/transaction/entity/cache/MerchantPayModeDownTimeCacheEntity.java
+spring.data.gemfire.cache.region.region-name=AGG_PayGateway_DownTime_DTLS
+spring.data.gemfire.logging.level=info
+
+logging.level.org.apache.geode=debug
+logging.level.org.springframework.data.gemfire=debug
