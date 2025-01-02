86663aa22589a37350c2352e4e7c49d0ce7f05b5
lohithmp <109272620+lohithmp@users.noreply.github.com>
2025-01-02 11:59:59 +0530

    Update test.java

--- test.java
+++ test.java
@@ -1,73 +1,23 @@
-private PaymentOptionsDTO getOtherPaymentOptions(List<MerchantPaymode> merchantPayModeList, Map<String, DownTimeDTO> downTimeDTOMap) {
-    // Initializing the lists for different payment modes
-    List<InbDTO> otherInbList = new ArrayList<>();
-    List<CardDTO> otherCcCardList = new ArrayList<>();
-    List<CardDTO> otherDcCardList = new ArrayList<>();
-    List<CardDTO> otherPcCardList = new ArrayList<>();
+start locator --name=locator1
+Starting a GemFire Locator in C:\Users\V1014352\Documents\vmware-gemfire-10.1.2\bin\locator1...
+The Locator process terminated unexpectedly with exit status 1. Please refer to the log file in C:\Users\V1014352\Documents\vmware-gemfire-10.1.2\bin\locator1 for full details.
 
-    // Lambda to map payModeCode to its corresponding list
-    Map<String, List<CardDTO>> payModeToCardListMap = Map.of(
-            ApplicationConstants.PAY_MODE_CODE_CREDITCARD, otherCcCardList,
-            ApplicationConstants.PAY_MODE_CODE_DEBITCARD, otherDcCardList,
-            ApplicationConstants.PAY_MODE_CODE_PREPAIDCARD, otherPcCardList
-    );
-
-    // Process merchantPayModeList
-    for (MerchantPaymode merchantPaymode : merchantPayModeList) {
-        String payModeCode = merchantPaymode.getPayModeCode();
-
-        if (payModeCode.equalsIgnoreCase(ApplicationConstants.PAY_MODE_CODE_NETBANKING)) {
-            otherInbList.add(InbDTO.builder()
-                    .bankId(merchantPaymode.getPayGatewayId())
-                    .bankName(merchantPaymode.getPayGatewayName())
-                    .payproctype(merchantPaymode.getPayProc())
-                    .payproc(merchantPaymode.getPayProc())
-                    .aggregatorGatewayMapId(merchantPaymode.getAggregatorGatewayMapId())
-                    .build());
-        } else {
-            List<CardDTO> cardList = payModeToCardListMap.get(payModeCode.toUpperCase());
-            if (cardList != null) {
-                cardList.add(CardDTO.builder()
-                        .bankName(merchantPaymode.getPayGatewayName())
-                        .payproctype(merchantPaymode.getPayProc())
-                        .aggregatorGatewayMapId(merchantPaymode.getAggregatorGatewayMapId())
-                        .payproc(merchantPaymode.getPayProc())
-                        .build());
-            }
-        }
-    }
-
-    // Process downTimeDTOMap
-    downTimeDTOMap.values().forEach(downTimeDTO -> {
-        String payModeCode = downTimeDTO.getPayModeCode();
-        if (payModeCode.equalsIgnoreCase(ApplicationConstants.PAY_MODE_CODE_NETBANKING)) {
-            otherInbList.add(InbDTO.builder().downtime(downTimeDTO).build());
-        } else {
-            List<CardDTO> cardList = payModeToCardListMap.get(payModeCode.toUpperCase());
-            if (cardList != null) {
-                cardList.add(CardDTO.builder().downtime(downTimeDTO).build());
-            }
-        }
-    });
-
-    // Construct CardsDTO
-    CardsDTO otherCardsDTO = CardsDTO.builder()
-            .cc(otherCcCardList)
-            .dc(otherDcCardList)
-            .pc(otherPcCardList)
-            .build();
-
-    // Construct UpiDTO
-    UpiDTO otherUpiDTO = UpiDTO.builder()
-            .apps(Arrays.asList(ApplicationConstants.PAY_MODE_DEFAULT_UPI_APPS))
-            .vpa(true)
-            .drawerIntent(true)
-            .build();
-
-    // Construct and return PaymentOptionsDTO
-    return PaymentOptionsDTO.builder()
-            .upi(otherUpiDTO)
-            .cards(otherCardsDTO)
-            .inb(otherInbList)
-            .build();
-        }
+Exception in thread "main" com.vmware.gemfire.deployment.modules.internal.LauncherException: java.lang.RuntimeException: An IO error occurred while starting a Locator in C:\Users\V1014352\Documents\vmware-gemfire-10.1.2\bin\locator1 on SBIEPAYBD173.CORP.AD.SBI[10334]: Network is unreachable; port (10334) is not available on localhost.
+    at gemfire//com.vmware.gemfire.deployment.modules.internal.Launcher.launch(Launcher.java:90)
+    at gemfire//com.vmware.gemfire.deployment.modules.internal.Launcher.main(Launcher.java:103)
+    at org.jboss.modules.Module.run(Module.java:353)
+    at org.jboss.modules.Module.run(Module.java:321)
+    at org.jboss.modules.Main.main(Main.java:604)
+    at com.vmware.gemfire.bootstrap.internal.Launcher.launch(Launcher.java:64)
+    at com.vmware.gemfire.bootstrap.LocatorLauncher.main(LocatorLauncher.java:31)
+Caused by: java.lang.RuntimeException: An IO error occurred while starting a Locator in C:\Users\V1014352\Documents\vmware-gemfire-10.1.2\bin\locator1 on SBIEPAYBD173.CORP.AD.SBI[10334]: Network is unreachable; port (10334) is not available on localhost.
+    at gemfire//org.apache.geode.distributed.LocatorLauncher.start(LocatorLauncher.java:808)
+    at gemfire//org.apache.geode.distributed.LocatorLauncher.run(LocatorLauncher.java:692)
+    at gemfire//org.apache.geode.distributed.LocatorLauncher.main(LocatorLauncher.java:229)
+    at gemfire//com.vmware.gemfire.deployment.modules.internal.Launcher.invokeMainClass(Launcher.java:99)
+    at gemfire//com.vmware.gemfire.deployment.modules.internal.Launcher.launch(Launcher.java:88)
+    ... 6 more
+Caused by: java.net.BindException: Network is unreachable; port (10334) is not available on localhost.
+    at gemfire//org.apache.geode.distributed.AbstractLauncher.assertPortAvailable(AbstractLauncher.java:152)
+    at gemfire//org.apache.geode.distributed.LocatorLauncher.start(LocatorLauncher.java:780)
+    ... 10 more
