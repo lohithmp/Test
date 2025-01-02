20fa2d2322dd782c3b339c92bd95929d20fe2e03
lohithmp <109272620+lohithmp@users.noreply.github.com>
2025-01-02 19:27:02 +0530

    Update test2.java

--- test2.java
+++ test2.java
@@ -1,42 +1,15 @@
-Parameter 0 of constructor in com.epay.transaction.dao.DowntimeAPIDao required a bean of type 'com.epay.transaction.repository.cache.DowntimeAPICacheRepository' that could not be found.
-
-
-Action:
-
-Consider defining a bean of type 'com.epay.transaction.repository.cache.DowntimeAPICacheRepository' in your configuration.
+PdxType[dsid=0,typenum=12009724,name=com.epay.admin.entity.cache.MerchantPayModeDownTimeCacheEntity,fields=[id:String:identity:0:idx0(relativeOffset)=0:idx1(vlfOffsetIndex)=-1, startTimestamp:String:1:1:idx0(relativeOffset)=0:idx1(vlfOffsetIndex)=1, endTimestamp:String:2:2:idx0(relativeOffset)=0:idx1(vlfOffsetIndex)=2, errorMessage:String:3:3:idx0(relativeOffset)=0:idx1(vlfOffsetIndex)=3, status:String:4:4:idx0(relativeOffset)=0:idx1(vlfOffsetIndex)=4, recordStatus:String:5:5:idx0(relativeOffset)=0:idx1(vlfOffsetIndex)=5, payModeCode:String:6:6:idx0(relativeOffset)=0:idx1(vlfOffsetIndex)=6, ]]
 
 
 
-	
-import com.epay.transaction.entity.cache.CustomerCacheEntity;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
+    public Optional<MerchantPayModeDownTimeCacheEntity> getDowntimeAPIs(String status) {
+//        List<MerchantPayModeDownTimeCacheEntity> downtimeAPIs = downtimeAPICacheRepository.findByStatusAndRecordStatus(status);
+        Optional<MerchantPayModeDownTimeCacheEntity> pdxInstance = downtimeAPICacheRepository.findById("15");
 
 
-@Repository
-public interface CustomerCacheRepository extends CrudRepository<CustomerCacheEntity, String> {
-}
-
-
-
-@Service
-@RequiredArgsConstructor
-public class DowntimeAPIService {
-
-//    private final DowntimeAPIDao downtimeAPIDao;
-    /**
-     * This methods contains transaction downtime api details.
-     *
-     * @return list of transaction downtime api details.
-     */
-
-//    private final DowntimeAPICacheRepository downtimeAPICacheRepository;
-    private final CustomerCacheRepository customerCacheRepository;
-
-
-    public Optional<CustomerCacheEntity> getCustomer() {
-        Optional<CustomerCacheEntity> cacheEntity = customerCacheRepository.findById("4");
-        return cacheEntity;
+        if (pdxInstance.isEmpty()) {
+            logger.info("Transaction downtime api details found empty in cache and called from database tables.");
+//            downtimeAPIs = adminServicesClient.getDowntimeAPIs();
+        }
+        return pdxInstance;
     }
-
-}
