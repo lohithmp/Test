7bfce52f7fb52057fe0cbb9bcdcb998d2755f0dd
lohithmp <109272620+lohithmp@users.noreply.github.com>
2025-01-02 17:11:49 +0530

    Update test2.java

--- test2.java
+++ test2.java
@@ -1,3 +1,42 @@
-Caused by: org.springframework.beans.factory.NoSuchBeanDefinitionException: No qualifying bean of type 'org.apache.geode.cache.Region<java.lang.String, com.epay.transaction.entity.cache.MerchantPayModeDownTimeCacheEntity>' available: expected at least 1 bean which qualifies as autowire candidate. Dependency annotations: {}
-	at org.springframework.beans.factory.support.DefaultListableBeanFactory.raiseNoMatchingBeanFound(DefaultListableBeanFactory.java:1880)
-Caused by: org.springframework.beans.factory.NoSuchBeanDefinitionException: No qualifying bean of type 'org.apache.geode.cache.Region<java.lang.String, com.epay.transaction.entity.cache.MerchantPayModeDownTimeCacheEntity>' available: expected at least 1 bean which qualifies as autowire candidate. Dependency annotations: {}
+Parameter 0 of constructor in com.epay.transaction.dao.DowntimeAPIDao required a bean of type 'com.epay.transaction.repository.cache.DowntimeAPICacheRepository' that could not be found.
+
+
+Action:
+
+Consider defining a bean of type 'com.epay.transaction.repository.cache.DowntimeAPICacheRepository' in your configuration.
+
+
+
+	
+import com.epay.transaction.entity.cache.CustomerCacheEntity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface CustomerCacheRepository extends CrudRepository<CustomerCacheEntity, String> {
+}
+
+
+
+@Service
+@RequiredArgsConstructor
+public class DowntimeAPIService {
+
+//    private final DowntimeAPIDao downtimeAPIDao;
+    /**
+     * This methods contains transaction downtime api details.
+     *
+     * @return list of transaction downtime api details.
+     */
+
+//    private final DowntimeAPICacheRepository downtimeAPICacheRepository;
+    private final CustomerCacheRepository customerCacheRepository;
+
+
+    public Optional<CustomerCacheEntity> getCustomer() {
+        Optional<CustomerCacheEntity> cacheEntity = customerCacheRepository.findById("4");
+        return cacheEntity;
+    }
+
+}
