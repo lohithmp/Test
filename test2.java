4729800b19c3b2c82c03d093ba9c86829d8dfeb0
lohithmp <109272620+lohithmp@users.noreply.github.com>
2025-01-29 15:57:28 +0530

    Update test2.java

--- test2.java
+++ test2.java
@@ -1,36 +1,23 @@
-package com.epay.transaction.config;
-
-import com.epay.transaction.entity.cache.MerchantPayModeDownTimeCacheEntity;
-import com.epay.transaction.repository.cache.DowntimeAPICacheRepository;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.gemfire.config.annotation.ClientCacheApplication;
-import org.springframework.data.gemfire.config.annotation.EnableEntityDefinedRegions;
-import org.springframework.data.gemfire.repository.config.EnableGemfireRepositories;
-
-
-@Configuration
-@ClientCacheApplication
-@EnableEntityDefinedRegions(basePackageClasses  = MerchantPayModeDownTimeCacheEntity.class)
-@EnableGemfireRepositories(basePackageClasses = DowntimeAPICacheRepository.class)
-public class GemFireConfiguration {}
-
-
-package com.epay.transaction.entity.cache;
+package com.epay.admin.entity.cache;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Id;
-import lombok.Data;
+import lombok.*;
 import org.springframework.data.gemfire.mapping.annotation.Region;
 
 import java.io.Serializable;
+import java.util.Objects;
 
+//@Data
+@Region("Refresh_DownTime_PayMode")
 @Data
-@Region("AGG_PayGateway_DownTime_DTLS")
-public class MerchantPayModeDownTimeCacheEntity implements Serializable {
+@AllArgsConstructor
+@NoArgsConstructor
+public class MerchantPayModeDownTimeSaveEntity implements Serializable {
 
     @Id
     @Column(name = "paygtwid")
-    private String id;
+    private String gatewayId;
 
     @Column(name = "downtimestartdatetime")
     private String startTimestamp;
@@ -49,88 +36,32 @@ public class MerchantPayModeDownTimeCacheEntity implements Serializable {
 
     @Column(name = "paymodecode")
     private String payModeCode;
-}
 
-package com.epay.transaction.repository.cache;
-
-import com.epay.transaction.entity.cache.MerchantPayModeDownTimeCacheEntity;
-import org.springframework.data.gemfire.repository.Query;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-import java.util.List;
-
-
-@Repository
-public interface DowntimeAPICacheRepository extends CrudRepository<MerchantPayModeDownTimeCacheEntity, String> {
-
-        @Query("SELECT payModeDownTime FROM /AGG_PayGateway_DownTime_DTLS payModeDownTime WHERE payModeDownTime.status = $1")
-        List<Object> findByStatusAndRecordStatus(String status);
-
-}
-
-
-package com.epay.transaction.dao;
-
-import com.epay.transaction.dto.DowntimeAPIDto;
-import com.epay.transaction.externalservice.AdminServicesClient;
-import com.epay.transaction.repository.cache.DowntimeAPICacheRepository;
-import com.sbi.epay.logging.utility.LoggerFactoryUtility;
-import com.sbi.epay.logging.utility.LoggerUtility;
-import lombok.AllArgsConstructor;
-import org.apache.geode.pdx.PdxInstance;
-import org.springframework.stereotype.Component;
-
-import java.util.List;
-import java.util.stream.Collectors;
-
-@Component
-@AllArgsConstructor
-public class DowntimeAPIDao {
-    private final LoggerUtility logger = LoggerFactoryUtility.getLogger(DowntimeAPIDao.class);
-    private final DowntimeAPICacheRepository downtimeAPICacheRepository;
-    private final AdminServicesClient adminServicesClient;
-
-    public List<DowntimeAPIDto> getDowntimeAPIs(String status) {
-        logger.info("Fetch the Transaction downtime details from cache region AGG_PayGateway_DownTime_DTLS.");
-        List<Object> objectList = downtimeAPICacheRepository.findByStatusAndRecordStatus(status);
-        if (objectList.isEmpty()) {
-            logger.info("Transaction downtime api details found empty in cache and called from database tables.");
-            return adminServicesClient.getDowntimeAPIs();
-        }
-        logger.info("Transaction downtime details from cache region AGG_PayGateway_DownTime_DTLS fetched successfully.");
-        return objectList.stream().map(this::convertToEntity).collect(Collectors.toList());
+    @Override
+    public int hashCode() {
+        return Objects.hash(gatewayId);
     }
 
-    private DowntimeAPIDto convertToEntity(Object pdxInstance) {
-        if (pdxInstance instanceof PdxInstance) {
-            logger.info("Transaction downtime details from cache is in type PdxInstance.");
-            return (DowntimeAPIDto) mapPdxInstanceToEntity((PdxInstance) pdxInstance);
-        } else if (pdxInstance instanceof DowntimeAPIDto) {
-            logger.info("Transaction downtime details from cache is in type Object type");
-            return (DowntimeAPIDto) pdxInstance;
-        }
-        return (DowntimeAPIDto) pdxInstance;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MerchantPayModeDownTimeSaveEntity that = (MerchantPayModeDownTimeSaveEntity) o;
+        return Objects.equals(gatewayId, that.gatewayId);
     }
 
-    public DowntimeAPIDto mapPdxInstanceToEntity(PdxInstance instance) {
-        logger.info("Mapping started for PdxInstance type to Object.");
-        DowntimeAPIDto entity = new DowntimeAPIDto();
-        entity.setPayGtwId((String) instance.getField("id"));
-        entity.setDownTimeStartDatetime((String) instance.getField("startTimestamp"));
-        entity.setDownTimeEndDatetime((String) instance.getField("endTimestamp"));
-        entity.setErrorMessage((String) instance.getField("errorMessage"));
-        entity.setStatus((String) instance.getField("status"));
-        entity.setRecordStatus((String) instance.getField("recordStatus"));
-        entity.setPayModeCode((String) instance.getField("payModeCode"));
-        return entity;
+    @Override
+    public String toString() {
+        return "MerchantPayModeDownTimeSaveEntity{" +
+                "gatewayId='" + gatewayId + '\'' +
+                ", hashCode=" + this.hashCode() +  // Include hashCode in output
+                '}';
     }
+
 }
 
 
-spring.data.gemfire.locators=localhost[10334]
-spring.data.gemfire.cache.server.port=40404
-spring.data.gemfire.logging.level=info
-logging.level.org.apache.geode=debug
-logging.level.org.springframework.data.gemfire=debug
-    spring.data.gemfire.pdx.read-serialized=false
+
+
+
+
