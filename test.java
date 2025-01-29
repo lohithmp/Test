8b8d27a6cf21f19b1e649107215ed3b6278a8236
lohithmp <109272620+lohithmp@users.noreply.github.com>
2025-01-29 14:28:23 +0530

    Update test.java

--- test.java
+++ test.java
@@ -1,97 +1,104 @@
-package com.epay.transaction.config;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.gemfire.mapping.annotation.Region;
 
-import com.epay.transaction.entity.cache.MerchantPayModeDownTimeCacheEntity;
-import com.epay.transaction.repository.cache.DowntimeAPICacheRepository;
-import org.apache.geode.cache.client.ClientCache;
-import org.apache.geode.cache.client.ClientRegionShortcut;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.gemfire.client.ClientRegionFactoryBean;
-import org.springframework.data.gemfire.config.annotation.ClientCacheApplication;
-import org.springframework.data.gemfire.config.annotation.EnableEntityDefinedRegions;
-import org.springframework.data.gemfire.repository.config.EnableGemfireRepositories;
+import java.util.Objects;
 
+@Region("MerchantRegion")  // Defines the region in GemFire
+public class MerchantEntity {
 
-@Configuration
-@ClientCacheApplication
-@EnableEntityDefinedRegions(basePackageClasses = MerchantPayModeDownTimeCacheEntity.class)
-@EnableGemfireRepositories(basePackageClasses = DowntimeAPICacheRepository.class)
-public class GemFireConfiguration {
+    @Id  // Primary Key
+    private String merchantId;
+    private String name;
+    private String country;
 
-    @Bean
-    public ClientRegionFactoryBean<MerchantPayModeDownTimeCacheEntity, String> merchantRegionFactory(ClientCache clientCache) {
-        ClientRegionFactoryBean<MerchantPayModeDownTimeCacheEntity, String> region = new ClientRegionFactoryBean<>();
-        region.setCache(clientCache);
-        region.setRegionName("AGG_PayGateway_DownTime_DTLS");
-        region.setShortcut(ClientRegionShortcut.PROXY);
-        return region;
+    // Constructors
+    public MerchantEntity() {}
+
+    public MerchantEntity(String merchantId, String name, String country) {
+        this.merchantId = merchantId;
+        this.name = name;
+        this.country = country;
+    }
+
+    // Getters and Setters
+    public String getMerchantId() { return merchantId; }
+    public void setMerchantId(String merchantId) { this.merchantId = merchantId; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public String getCountry() { return country; }
+    public void setCountry(String country) { this.country = country; }
+
+    // Overriding equals()
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MerchantEntity that = (MerchantEntity) o;
+        return Objects.equals(merchantId, that.merchantId);
+    }
+
+    // Overriding hashCode()
+    @Override
+    public int hashCode() {
+        return Objects.hash(merchantId);
+    }
+
+    @Override
+    public String toString() {
+        return "MerchantEntity{" +
+                "merchantId='" + merchantId + '\'' +
+                ", name='" + name + '\'' +
+                ", country='" + country + '\'' +
+                ", hashCode=" + this.hashCode() +  // Include hashCode in output
+                '}';
     }
 }
-Step 1:
-		I make the spring.data.gemfire.pdx.read-serialized=false I will face this error
-		
-		Implementation:
-			spring.data.gemfire.pdx.read-serialized=false
-		Error:
-			org.springframework.dao.DataAccessResourceFailureException: remote server on SBIEPAYBD173(SpringBasedClientCacheApplication:7672:loner):53067:dae10297:SpringBasedClientCacheApplication: 
-			org.apache.geode.SerializationException: While deserializing query result
 
 
-Step 2:
-		I have tried to use the ReflectionBasedAutoSerializer but still faced the same error 
-
-		Implementation: 
-			@Configuration
-			@EnableEntityDefinedRegions(basePackageClasses = MerchantPayModeDownTimeCacheEntity.class)
-			@EnableGemfireRepositories(basePackageClasses = DowntimeAPICacheRepository.class)
-			public class GemFireConfiguration {
-
-				@Bean("gemfireCache")
-				public ClientCache clientCache() {
-					return new ClientCacheFactory()
-							.setPdxReadSerialized(false)
-							.setPdxSerializer(new ReflectionBasedAutoSerializer("com.epay.transaction.entity.cache.*"))
-							.create();
-				}
-			}
-
-		Error:
-			remote server on SBIEPAYBD173(25464:loner):56714:21f99597: org.apache.geode.SerializationException: While deserializing query result
 
 
-Step 3: I have enabled the PdxReadSerialized but I will get the data in pdxInstance format. Right now we are handling pdxInstance by mapping the data.
+import org.springframework.data.gemfire.repository.GemfireRepository;
+import org.springframework.stereotype.Repository;
 
-		Implementation: 
-			spring.data.gemfire.pdx.read-serialized=true
+@Repository
+public interface MerchantRepository extends GemfireRepository<MerchantEntity, String> {
+}
 
-		Error:
-			org.springframework.dao.DataAccessResourceFailureException: remote server on SBIEPAYBD173(SpringBasedClientCacheApplication:7672:loner):53067:dae10297:SpringBasedClientCacheApplication: 
-			org.apache.geode.SerializationException: While deserializing query result
 
-Step 4: I have followed MappingPdxSerializer and annotation EnablePdx but here also facing the same error
 
-		Implementation:
-			@Configuration
-			@ClientCacheApplication
-			@EnableEntityDefinedRegions(basePackageClasses = MerchantPayModeDownTimeCacheEntity.class)
-			@EnableGemfireRepositories(basePackageClasses = DowntimeAPICacheRepository.class)
-			@EnablePdx(serializerBeanName = "myCustomMappingPdxSerializer")
-			public class GemFireConfiguration {
 
-				@Bean
-				MappingPdxSerializer myCustomMappingPdxSerializer() {
 
-					MappingPdxSerializer customMappingPdxSerializer =
-							MappingPdxSerializer.newMappingPdxSerializer();
 
-					customMappingPdxSerializer.setIncludeTypeFilters(
-							type -> Principal.class.isAssignableFrom(type));
 
-					return customMappingPdxSerializer;
-				}
-			}
-			
-		Error:
-			org.springframework.dao.DataAccessResourceFailureException: remote server on SBIEPAYBD173(SpringBasedClientCacheApplication:7672:loner):53067:dae10297:SpringBasedClientCacheApplication: 
-			org.apache.geode.SerializationException: While deserializing query result
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/merchant")
+public class MerchantController {
+
+    @Autowired
+    private MerchantRepository merchantRepository;
+
+    // Insert Merchant Data
+    @PostMapping("/add")
+    public MerchantEntity addMerchant(@RequestBody MerchantEntity merchant) {
+        merchantRepository.save(merchant);
+        return merchant;  // Returns saved entity with hashCode included in toString()
+    }
+
+    // Get Merchant by ID
+    @GetMapping("/{id}")
+    public Optional<MerchantEntity> getMerchant(@PathVariable String id) {
+        return merchantRepository.findById(id);
+    }
+
+    // Get hashCode of a Merchant by ID
+    @GetMapping("/hashcode/{id}")
+    public String getMerchantHashCode(@PathVariable String id) {
+        Optional<MerchantEntity> merchant = merchantRepository.findById(id);
+        return merchant.map(value -> "HashCode: " + value.hashCode()).orElse("Merchant not found");
+    }
+}
