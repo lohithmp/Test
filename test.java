7fe283ecad0394729bc9a978fa52b61fe7e3f9e4
lohithmp <109272620+lohithmp@users.noreply.github.com>
2025-01-30 14:41:42 +0530

    Update test.java

--- test.java
+++ test.java
@@ -1,104 +1,65 @@
-import org.springframework.data.annotation.Id;
-import org.springframework.data.gemfire.mapping.annotation.Region;
 
-import java.util.Objects;
+GemFire Local Setup Instruction
 
-@Region("MerchantRegion")  // Defines the region in GemFire
-public class MerchantEntity {
+Add Properties in gradle.properties
+Add These properties in gradle.properties file which is inside .gradle folder.
 
-    @Id  // Primary Key
-    private String merchantId;
-    private String name;
-    private String country;
+systemProp.http.nonProxyHosts=gitlab.epay.sbi
+systemProp.https.nonProxyHosts=gitlab.epay.sbi
+gitlab.username=<PASS_YOUR_ADID or GITLAB_USERNAME>
+gitlab.token=<PASS_YOUR_GITALAB_TOKEN>
 
-    // Constructors
-    public MerchantEntity() {}
+						or
+use gradle.properties file in your .gradle folder and change the gitlab credentials
+https://neosofttechnologiesmail-my.sharepoint.com/personal/binoy_medhi_neosofttech_com/_layouts/15/onedrive.aspx?id=%2Fpersonal%2Fbinoy%5Fmedhi%5Fneosofttech%5Fcom%2FDocuments%2FCedge%2FOthers%2FGemFire%2Fgradle%2Eproperties&parent=%2Fpersonal%2Fbinoy%5Fmedhi%5Fneosofttech%5Fcom%2FDocuments%2FCedge%2FOthers%2FGemFire
 
-    public MerchantEntity(String merchantId, String name, String country) {
-        this.merchantId = merchantId;
-        this.name = name;
-        this.country = country;
-    }
+GemFire Local Server Setup
+Go to Drive inside go to this directory -- Binoy Medhi>Cedge>Others>Gemfire
+or
+ download it from below link
+https://neosofttechnologiesmail-my.sharepoint.com/personal/binoy_medhi_neosofttech_com/_layouts/15/onedrive.aspx?id=%2Fpersonal%2Fbinoy%5Fmedhi%5Fneosofttech%5Fcom%2FDocuments%2FCedge%2FOthers%2FGemFire
+Download the GireFire_local_server.zip file extract it.
 
-    // Getters and Setters
-    public String getMerchantId() { return merchantId; }
-    public void setMerchantId(String merchantId) { this.merchantId = merchantId; }
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-    public String getCountry() { return country; }
-    public void setCountry(String country) { this.country = country; }
+Go Inside the Extracted Folder and open bin folder directory
+take a terminal from bin directory
+Run the cmd from GemFire bin directory
+below are the commands to connect with locators, server and so on.
 
-    // Overriding equals()
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        MerchantEntity that = (MerchantEntity) o;
-        return Objects.equals(merchantId, that.merchantId);
-    }
+Commands for Start the locator and server
 
-    // Overriding hashCode()
-    @Override
-    public int hashCode() {
-        return Objects.hash(merchantId);
-    }
-
-    @Override
-    public String toString() {
-        return "MerchantEntity{" +
-                "merchantId='" + merchantId + '\'' +
-                ", name='" + name + '\'' +
-                ", country='" + country + '\'' +
-                ", hashCode=" + this.hashCode() +  // Include hashCode in output
-                '}';
-    }
-}
+ To connect with gemfire
+	> gfsh
+To start the locator
+	>  start locator --name=locator 
+			or
+	   (If above command is not worked, Try with this command)
+	 > start locator --name=locator --bind-address=127.0.0.1
+To start the server
+	> start server --name=server
+			or				
+	(If above command is not worked, Try with this command)
+	 > start server --name=server --bind-address=127.0.0.1
+To create the region
+	> create region --name=<REGION_NAME> --type=REPLICATE
+To fetch the data from specific region
+	> query --query="select * from /<REGION_NAME>"
 
 
 
 
-import org.springframework.data.gemfire.repository.GemfireRepository;
-import org.springframework.stereotype.Repository;
+GemFire Official Documents Go Through links 
 
-@Repository
-public interface MerchantRepository extends GemfireRepository<MerchantEntity, String> {
-}
+Link for GemFire solutions in spring boot:
+https://techdocs.broadcom.com/us/en/vmware-tanzu/data-solutions/spring-boot-for-tanzu-gemfire/2-0/gf-sb-2-0/index.html
 
 
+Link for GemFire document:
+https://techdocs.broadcom.com/us/en/vmware-tanzu/data-solutions/tanzu-gemfire/10-1/gf/about_gemfire.html
 
 
+Link for OQL document interacting with GemFire:
+https://gemfire.dev/tutorials/java/oql/
 
+Github link for GemFire spring boot:
+https://github.com/spring-projects/spring-data-gemfire/tree/main/src/main/java/org/springframework/data/gemfire
 
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.Optional;
-
-@RestController
-@RequestMapping("/merchant")
-public class MerchantController {
-
-    @Autowired
-    private MerchantRepository merchantRepository;
-
-    // Insert Merchant Data
-    @PostMapping("/add")
-    public MerchantEntity addMerchant(@RequestBody MerchantEntity merchant) {
-        merchantRepository.save(merchant);
-        return merchant;  // Returns saved entity with hashCode included in toString()
-    }
-
-    // Get Merchant by ID
-    @GetMapping("/{id}")
-    public Optional<MerchantEntity> getMerchant(@PathVariable String id) {
-        return merchantRepository.findById(id);
-    }
-
-    // Get hashCode of a Merchant by ID
-    @GetMapping("/hashcode/{id}")
-    public String getMerchantHashCode(@PathVariable String id) {
-        Optional<MerchantEntity> merchant = merchantRepository.findById(id);
-        return merchant.map(value -> "HashCode: " + value.hashCode()).orElse("Merchant not found");
-    }
-}
