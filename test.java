b93095fc4913769bd94d8b7b129627ae6b9bb550
lohithmp <109272620+lohithmp@users.noreply.github.com>
2025-01-30 15:10:12 +0530

    Update test.java

--- test.java
+++ test.java
@@ -1,65 +1,129 @@
+GemFire Local Setup Guide
 
-GemFire Local Setup Instruction
+This guide provides step-by-step instructions for setting up GemFire locally, configuring Gradle properties, and running a GemFire server.
 
-Add Properties in gradle.properties
-Add These properties in gradle.properties file which is inside .gradle folder.
+
+---
+
+1. Configuring Gradle Properties
+
+To set up GemFire with Gradle, update the gradle.properties file inside the .gradle folder.
+
+Steps:
+
+1. Open the .gradle folder in your system.
+
+
+2. Locate the gradle.properties file (or create one if it does not exist).
+
+
+3. Add the following properties to the file:
 
 systemProp.http.nonProxyHosts=gitlab.epay.sbi
 systemProp.https.nonProxyHosts=gitlab.epay.sbi
-gitlab.username=<PASS_YOUR_ADID or GITLAB_USERNAME>
-gitlab.token=<PASS_YOUR_GITALAB_TOKEN>
+gitlab.username=<YOUR_ADID_OR_GITLAB_USERNAME>
+gitlab.token=<YOUR_GITLAB_TOKEN>
 
-						or
-use gradle.properties file in your .gradle folder and change the gitlab credentials
-https://neosofttechnologiesmail-my.sharepoint.com/personal/binoy_medhi_neosofttech_com/_layouts/15/onedrive.aspx?id=%2Fpersonal%2Fbinoy%5Fmedhi%5Fneosofttech%5Fcom%2FDocuments%2FCedge%2FOthers%2FGemFire%2Fgradle%2Eproperties&parent=%2Fpersonal%2Fbinoy%5Fmedhi%5Fneosofttech%5Fcom%2FDocuments%2FCedge%2FOthers%2FGemFire
 
-GemFire Local Server Setup
-Go to Drive inside go to this directory -- Binoy Medhi>Cedge>Others>Gemfire
-or
- download it from below link
-https://neosofttechnologiesmail-my.sharepoint.com/personal/binoy_medhi_neosofttech_com/_layouts/15/onedrive.aspx?id=%2Fpersonal%2Fbinoy%5Fmedhi%5Fneosofttech%5Fcom%2FDocuments%2FCedge%2FOthers%2FGemFire
-Download the GireFire_local_server.zip file extract it.
-
-Go Inside the Extracted Folder and open bin folder directory
-take a terminal from bin directory
-Run the cmd from GemFire bin directory
-below are the commands to connect with locators, server and so on.
-
-Commands for Start the locator and server
-
- To connect with gemfire
-	> gfsh
-To start the locator
-	>  start locator --name=locator 
-			or
-	   (If above command is not worked, Try with this command)
-	 > start locator --name=locator --bind-address=127.0.0.1
-To start the server
-	> start server --name=server
-			or				
-	(If above command is not worked, Try with this command)
-	 > start server --name=server --bind-address=127.0.0.1
-To create the region
-	> create region --name=<REGION_NAME> --type=REPLICATE
-To fetch the data from specific region
-	> query --query="select * from /<REGION_NAME>"
+4. Alternatively, you can use a pre-configured gradle.properties file from the following link and update it with your credentials:
+Download gradle.properties
 
 
 
 
-GemFire Official Documents Go Through links 
+---
 
-Link for GemFire solutions in spring boot:
-https://techdocs.broadcom.com/us/en/vmware-tanzu/data-solutions/spring-boot-for-tanzu-gemfire/2-0/gf-sb-2-0/index.html
+2. Setting Up a Local GemFire Server
+
+Downloading the GemFire Server Files
+
+1. Navigate to the following directory on your system:
+
+Binoy Medhi > Cedge > Others > GemFire
+
+OR
 
 
-Link for GemFire document:
-https://techdocs.broadcom.com/us/en/vmware-tanzu/data-solutions/tanzu-gemfire/10-1/gf/about_gemfire.html
+2. Download the GemFire server files from the link below:
+Download GemFire_local_server.zip
 
 
-Link for OQL document interacting with GemFire:
-https://gemfire.dev/tutorials/java/oql/
+3. Extract the GemFire_local_server.zip file.
 
-Github link for GemFire spring boot:
-https://github.com/spring-projects/spring-data-gemfire/tree/main/src/main/java/org/springframework/data/gemfire
+
+4. Open the extracted folder and navigate to the bin directory.
+
+
+5. Open a terminal in the bin directory.
+
+
+
+
+---
+
+3. Running GemFire Locally
+
+Starting GemFire Services
+
+1. Open the GemFire shell (GFSH)
+
+gfsh
+
+
+2. Start the locator
+
+start locator --name=locator
+
+OR (if the above command does not work)
+
+start locator --name=locator --bind-address=127.0.0.1
+
+
+3. Start the server
+
+start server --name=server
+
+OR (if the above command does not work)
+
+start server --name=server --bind-address=127.0.0.1
+
+
+
+Creating and Querying a Region
+
+1. Create a region
+
+create region --name=<REGION_NAME> --type=REPLICATE
+
+
+2. Query data from a specific region
+
+query --query="SELECT * FROM /<REGION_NAME>"
+
+
+
+
+---
+
+4. Official GemFire Documentation & Resources
+
+Spring Boot Integration with GemFire:
+Spring Boot for Tanzu GemFire
+
+GemFire Official Documentation:
+Tanzu GemFire Documentation
+
+OQL (Object Query Language) Guide for GemFire:
+GemFire OQL Tutorial
+
+Spring Boot GemFire GitHub Repository:
+Spring Data GemFire GitHub
+
+
+
+---
+
+Conclusion
+
+Following this guide, you should be able to set up and run a GemFire server locally, configure Gradle properties, create regions, and query data efficiently. For further support, refer to the official documentation provided above.
 
