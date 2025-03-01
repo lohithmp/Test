131c5ce309679d57bc79dcbbe1bae4ff1f4b3604
lohithmp <109272620+lohithmp@users.noreply.github.com>
2025-03-01 13:46:32 +0530

    Update test.java

--- test.java
+++ test.java
@@ -1,3 +1,33 @@
-https://github.com/Tanzu-Solutions-Engineering/tanzu-rabbitmq-event-streaming-showcase/blob/main/docs/workshops/Labs/Data-Transformation/labs/lab-ingestion/01-Postgres-CDC-TO-GemFire.md
+Implement a scheduler named GatewayPoolingService that will run every hour to process merchant order payments for corporate.
+Scheduler execute every 2 minutes.
+Requirement:
 
-https://spring.io/blog/2020/12/14/case-study-change-data-capture-cdc-analysis-with-cdc-debezium-source-and-analytics-sink-in-real-time
+
+        
+      
+Fetch records from Table: MERCHANT_ORDER_PAYMENTS, where condition = TRASACTION_STATUS="BOOKED" or "PENDING" AND PAYMENT_STATUS="PAYMENT_INITIATION_START" or "PENDING".
+It will return multiple transaction (ATRN) records.
+
+
+        
+      
+Process transaction records using Spring Batch framework: Use Spring Batch to process the records retrieved from table: MERCHANT_ORDER_PAYMENTS. i.e. Extract multiple ATRNs along with their status and push to the KAFKA publisher.
+
+
+        
+      
+KAFKA Consumer Handling: It consumes the Kafka messages and initiate the next process i.e. Call API based on ATRN Pay Mode.
+
+
+        
+      
+External API Call - Payment Service API: If the transaction (ATRN) will be processed as per paymode i.e.
+Paymode is = "INB" → Call the Payment Service INB API.
+Paymode is = "OtherINB" → Call the Payment Service other INB API.
+
+
+        
+      
+Retry Logic: In case of transaction failed to process,
+If STATUS="PENDING" then it will retry for 24 hours. interval can be added 3 minutes hardcoded now.
+If STATUS="BOOKING" then it will retry based on start time defined value in admin(For now hardcode 7 minutes). interval can be added 3 minutes hardcoded for now.
