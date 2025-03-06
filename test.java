7cf13ce1b0cdc969a579c2fcc74373772593169c
lohithmp <109272620+lohithmp@users.noreply.github.com>
2025-03-06 18:14:01 +0530

    Update test.java

--- test.java
+++ test.java
@@ -10,24 +10,24 @@
         "database.server.name": "epaydbdev1",
         "schema.include.list": "LOHITH_M",
         "table.include.list": "LOHITH_M.bankbranches, LOHITH_M.AGGMERCHGTWFEESTRUCTURE, LOHITH_M.SERVICETAXSTRUCTURE, LOHITH_M.AGGMERCHANTSERVICETAXMAPPING, LOHITH_M.AGGMERCHANTHYBRIDFEESTRUCTURE",
-        "database.history.kafka.bootstrap.servers": "localhost:9092",
-        "database.history.kafka.topic": "schema.changes",
+        // "database.history.kafka.bootstrap.servers": "localhost:9092",
+        // "database.history.kafka.topic": "schema.changes",
         "topic.prefix": "Admin",
-        "task.max": "1",
+        // "task.max": "1",
         "schema.history.internal.kafka.bootstrap.servers": "localhost:9092",
         "schema.history.internal.kafka.topic": "schema.changes",
 
-        "database.history.store.only.captured.tables.ddl": true,
-        "include.schema.changes":false,
+        // "database.history.store.only.captured.tables.ddl": true,
+        // "include.schema.changes":true,
 
-        "value.converter.schemas.enable": false,
+        // "value.converter.schemas.enable": false,
         "datetime.handling.mode": "string",
         "decimal.handling.mode": "string",
-        "binary.handling.mode": "hex",
+        // "binary.handling.mode": "hex",
 
-        "schema.exclude.list": "ROHINI_K, SHIVANI_R, SHUBHANGI_KURELAY, APEX_180200, HRISHIKESH_PANDIRKAR, BHOOPENDRA_RAJPUT, RAHUL_KUMAR, NIRVAY_BIKRAM, TEST_USER, PANDURANG, SHITAL_SURYAWANSHI, VINOD_BHOSLE, GIREESH_M, AARTI, PAYAGGADMIN, SANDEEP_DESHMUKH, POONAM_R, RAVI_RATHORE, BHUSHAN_WADEKAR"
+        "include.unchanged.toast": "true"
+        // "snapshot.mode": "no_data",
+
+        // "schema.exclude.list": "ROHINI_K, SHIVANI_R, SHUBHANGI_KURELAY, APEX_180200, HRISHIKESH_PANDIRKAR, BHOOPENDRA_RAJPUT, RAHUL_KUMAR, NIRVAY_BIKRAM, TEST_USER, PANDURANG, SHITAL_SURYAWANSHI, VINOD_BHOSLE, GIREESH_M, AARTI, PAYAGGADMIN, SANDEEP_DESHMUKH, POONAM_R, RAVI_RATHORE, BHUSHAN_WADEKAR"
     }
 }
-
-
-{"before":{"MERCHGTWFEEID":"111112","AGGREGATORID":"","MERCHANTID":"","PAYGTWID":"","OPERATINGMODEID":"","COUNTRYCODE":"","CURRENCYCODE":"","PAYMODECODE":"","PAYPROCID":"","ACCESSMEDIUMCODE":"","PAYPROCTYPE":"","AGGGTWMAPID":"","INSTRUCTIONTYPE":"","TRANSACTIONTYPE":"","FEEPROCESSFLAG":"","SLABFROM":"1.00","SLABTO":"0.00","MERCHANTFEEAPPLICABLE":"","MERCHANTFEETYPE":"","MERCHANTFEE":"0.00","OTHERFEEAPPLICABLE":"","OTHERFEETYPE":"","OTHERFEE":"0.0000","GTWFEEAPPLICABLE":"","GTWFEETYPE":"","GTWFEE":"0.00","AGGSERVICEFEEAPPLICABLE":"","AGGSERVICEFEETYPE":"","AGGSERVICEFEE":"0.00","STATUS":"","RECORDSTATUS":"A","MAKERREMARKS":null,"CHECKERREMARKS":null,"CREATIONDATE":0,"CREATEDBY":"","CREATEDBYSESSIONID":"","MODIFIEDDATE":null,"MODIFIEDBY":null,"MODIFIEDBYSESSIONID":null,"ADJUSTMENTFLAG":null},"after":{"MERCHGTWFEEID":"111112","AGGREGATORID":"","MERCHANTID":"","PAYGTWID":"","OPERATINGMODEID":"","COUNTRYCODE":"","CURRENCYCODE":"","PAYMODECODE":"","PAYPROCID":"","ACCESSMEDIUMCODE":"","PAYPROCTYPE":"","AGGGTWMAPID":"","INSTRUCTIONTYPE":"","TRANSACTIONTYPE":"","FEEPROCESSFLAG":"","SLABFROM":"3.00","SLABTO":"0.00","MERCHANTFEEAPPLICABLE":"","MERCHANTFEETYPE":"","MERCHANTFEE":"0.00","OTHERFEEAPPLICABLE":"","OTHERFEETYPE":"","OTHERFEE":"0.0000","GTWFEEAPPLICABLE":"","GTWFEETYPE":"","GTWFEE":"0.00","AGGSERVICEFEEAPPLICABLE":"","AGGSERVICEFEETYPE":"","AGGSERVICEFEE":"0.00","STATUS":"","RECORDSTATUS":"A","MAKERREMARKS":null,"CHECKERREMARKS":null,"CREATIONDATE":0,"CREATEDBY":"","CREATEDBYSESSIONID":"","MODIFIEDDATE":null,"MODIFIEDBY":null,"MODIFIEDBYSESSIONID":null,"ADJUSTMENTFLAG":null},"source":{"version":"3.0.6.Final","connector":"oracle","name":"Admin","ts_ms":1741182499000,"snapshot":"false","db":"EPAYDBDEV1","sequence":null,"ts_us":1741182499000000,"ts_ns":1741182499000000000,"schema":"LOHITH_M","table":"AGGMERCHGTWFEESTRUCTURE","txId":"0500080076ca0000","scn":"289815835","commit_scn":"289815836","lcr_position":null,"rs_id":"0x0034b1.00143517.0010","ssn":0,"redo_thread":1,"user_name":"LOHITH_M","redo_sql":null,"row_id":"AAAkvCAAIAAFqf/AAD"},"transaction":null,"op":"u","ts_ms":1741182507144,"ts_us":1741182507144726,"ts_ns":1741182507144726700}
