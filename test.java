f89c293008b0f889b4b6dcb697dfe44ed902f217
lohithmp <109272620+lohithmp@users.noreply.github.com>
2025-03-13 17:21:02 +0530

    Update test.java

--- test.java
+++ test.java
@@ -1,33 +1,44 @@
-{
-    "name": "admin-service-connector",
-    "config": {
-        "connector.class": "io.debezium.connector.oracle.OracleConnector",
-        "database.hostname": "10.177.134.124",
-        "database.port": "1590",
-        "database.user": "LOHITH_M",
-        "database.password": "LOHITH_M",
-        "database.dbname": "epaydbdev1",
-        "database.server.name": "epaydbdev1",
-        "schema.include.list": "LOHITH_M",
-        "table.include.list": "LOHITH_M.bankbranches, LOHITH_M.AGGMERCHGTWFEESTRUCTURE, LOHITH_M.SERVICETAXSTRUCTURE, LOHITH_M.AGGMERCHANTSERVICETAXMAPPING, LOHITH_M.AGGMERCHANTHYBRIDFEESTRUCTURE",
-        // "database.history.kafka.bootstrap.servers": "localhost:9092",
-        // "database.history.kafka.topic": "schema.changes",
-        "topic.prefix": "Admin",
-        // "task.max": "1",
-        "schema.history.internal.kafka.bootstrap.servers": "localhost:9092",
-        "schema.history.internal.kafka.topic": "schema.changes",
+ @KafkaListener(topics = "Admin.LOHITH_M.AGGMERCHGTWFEESTRUCTURE", groupId = "bankbranches-group")
+    public void processAggMerchGtwFeeStructure(Message<String> message) {
+        try {
+            logger.info("Received message: {}", message.getPayload());
+            JsonNode event = objectMapper.readTree(message.getPayload());
+            String operation = event.get("op").asText();
+            JsonNode after = event.get("after");
+            JsonNode before = event.get("before");
 
-        // "database.history.store.only.captured.tables.ddl": true,
-        // "include.schema.changes":true,
+            if (("c".equals(operation) || "u".equals(operation) || "r".equals(operation)) && after != null) {
+                AggMerchantGtwFeeStructureDto merchantGtwFeeStructureDto = objectMapper.treeToValue(after, AggMerchantGtwFeeStructureDto.class);
+                Optional<MerchantPricing> merchantPricing = merchantPricingRepository.getPricingByMerchantGwtFeeId(merchantGtwFeeStructureDto.getMerchantGtwFeeId(),
+                        merchantGtwFeeStructureDto.getMId(),
+                        merchantGtwFeeStructureDto.getPayProcType(),
+                        merchantGtwFeeStructureDto.getPayGtwId());
 
-        // "value.converter.schemas.enable": false,
-        "datetime.handling.mode": "string",
-        "decimal.handling.mode": "string",
-        // "binary.handling.mode": "hex",
+                if (merchantPricing.isEmpty()) {
+                    logger.info("Incoming message AGGMERCHGTWFEESTRUCTURE record is not found in View, PK-MERCHGTWFEEID: {}", merchantGtwFeeStructureDto.getMerchantGtwFeeId());
+                    return;
+                }
+                MerchantPricingEntity merchantPricingCacheEntity = new MerchantPricingEntity();
+                BeanUtils.copyProperties(merchantPricing.get(), merchantPricingCacheEntity);
 
-        "include.unchanged.toast": "true"
-        // "snapshot.mode": "no_data",
-
-        // "schema.exclude.list": "ROHINI_K, SHIVANI_R, SHUBHANGI_KURELAY, APEX_180200, HRISHIKESH_PANDIRKAR, BHOOPENDRA_RAJPUT, RAHUL_KUMAR, NIRVAY_BIKRAM, TEST_USER, PANDURANG, SHITAL_SURYAWANSHI, VINOD_BHOSLE, GIREESH_M, AARTI, PAYAGGADMIN, SANDEEP_DESHMUKH, POONAM_R, RAVI_RATHORE, BHUSHAN_WADEKAR"
+                Optional<MerchantPricingEntity> merchantPricingCache = merchantPricingService.getCacheMerchantPricing(merchantGtwFeeStructureDto.getMerchantGtwFeeId());
+                if (merchantPricingCache.isEmpty()) {
+                    logger.info("Incoming message inserted MerchantPricing record is saved cache, PK-MERCHGTWFEEID: {}", merchantGtwFeeStructureDto.getMerchantGtwFeeId());
+                    merchantPricingService.saveMerchantPricing(merchantPricingCacheEntity);
+                    return;
+                }
+                merchantPricingService.saveMerchantPricing(merchantPricingCacheEntity);
+            } else if ("d".equals(operation)) {
+                AggMerchantGtwFeeStructureDto aggMerchantGtwFeeStructureDto = objectMapper.treeToValue(before, AggMerchantGtwFeeStructureDto.class);
+                Optional<MerchantPricingEntity> merchantPricingCache = merchantPricingService.getCacheMerchantPricing(aggMerchantGtwFeeStructureDto.getMerchantGtwFeeId());
+                if (merchantPricingCache.isPresent()) {
+                    logger.info("AGGMERCHGTWFEESTRUCTURE record is deleting from Cache, PK-MERCHGTWFEEID: {}", aggMerchantGtwFeeStructureDto.getMerchantGtwFeeId());
+                    merchantPricingService.deleteCacheMerchantPricing(aggMerchantGtwFeeStructureDto.getMerchantGtwFeeId());
+                } else {
+                    logger.info("AGGMERCHGTWFEESTRUCTURE record is not found in Cache, PK-MERCHGTWFEEID: {}", aggMerchantGtwFeeStructureDto.getMerchantGtwFeeId());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-}
