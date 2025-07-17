fcd4498719fb40cc2ae2e29ba6c919f9970a0617
lohithmp <109272620+lohithmp@users.noreply.github.com>
2025-07-17 15:13:19 +0530

    Update test.java

--- test.java
+++ test.java
@@ -1,44 +1,67 @@
- @KafkaListener(topics = "Admin.LOHITH_M.AGGMERCHGTWFEESTRUCTURE", groupId = "bankbranches-group")
-    public void processAggMerchGtwFeeStructure(Message<String> message) {
+
+    private List<CardOnboardingCache> fetchCardOnBoardDetails(String mId) {
         try {
-            logger.info("Received message: {}", message.getPayload());
-            JsonNode event = objectMapper.readTree(message.getPayload());
-            String operation = event.get("op").asText();
-            JsonNode after = event.get("after");
-            JsonNode before = event.get("before");
-
-            if (("c".equals(operation) || "u".equals(operation) || "r".equals(operation)) && after != null) {
-                AggMerchantGtwFeeStructureDto merchantGtwFeeStructureDto = objectMapper.treeToValue(after, AggMerchantGtwFeeStructureDto.class);
-                Optional<MerchantPricing> merchantPricing = merchantPricingRepository.getPricingByMerchantGwtFeeId(merchantGtwFeeStructureDto.getMerchantGtwFeeId(),
-                        merchantGtwFeeStructureDto.getMId(),
-                        merchantGtwFeeStructureDto.getPayProcType(),
-                        merchantGtwFeeStructureDto.getPayGtwId());
-
-                if (merchantPricing.isEmpty()) {
-                    logger.info("Incoming message AGGMERCHGTWFEESTRUCTURE record is not found in View, PK-MERCHGTWFEEID: {}", merchantGtwFeeStructureDto.getMerchantGtwFeeId());
-                    return;
-                }
-                MerchantPricingEntity merchantPricingCacheEntity = new MerchantPricingEntity();
-                BeanUtils.copyProperties(merchantPricing.get(), merchantPricingCacheEntity);
-
-                Optional<MerchantPricingEntity> merchantPricingCache = merchantPricingService.getCacheMerchantPricing(merchantGtwFeeStructureDto.getMerchantGtwFeeId());
-                if (merchantPricingCache.isEmpty()) {
-                    logger.info("Incoming message inserted MerchantPricing record is saved cache, PK-MERCHGTWFEEID: {}", merchantGtwFeeStructureDto.getMerchantGtwFeeId());
-                    merchantPricingService.saveMerchantPricing(merchantPricingCacheEntity);
-                    return;
-                }
-                merchantPricingService.saveMerchantPricing(merchantPricingCacheEntity);
-            } else if ("d".equals(operation)) {
-                AggMerchantGtwFeeStructureDto aggMerchantGtwFeeStructureDto = objectMapper.treeToValue(before, AggMerchantGtwFeeStructureDto.class);
-                Optional<MerchantPricingEntity> merchantPricingCache = merchantPricingService.getCacheMerchantPricing(aggMerchantGtwFeeStructureDto.getMerchantGtwFeeId());
-                if (merchantPricingCache.isPresent()) {
-                    logger.info("AGGMERCHGTWFEESTRUCTURE record is deleting from Cache, PK-MERCHGTWFEEID: {}", aggMerchantGtwFeeStructureDto.getMerchantGtwFeeId());
-                    merchantPricingService.deleteCacheMerchantPricing(aggMerchantGtwFeeStructureDto.getMerchantGtwFeeId());
-                } else {
-                    logger.info("AGGMERCHGTWFEESTRUCTURE record is not found in Cache, PK-MERCHGTWFEEID: {}", aggMerchantGtwFeeStructureDto.getMerchantGtwFeeId());
-                }
-            }
+            return mapPdxList(cardOnboardingCacheRepository.findBymId(mId));
         } catch (Exception e) {
-            e.printStackTrace();
+            logger.error("GemFire cache lookup failed or GemFire server down. Reason: {}", e.getMessage());
         }
+        return Collections.emptyList();
+    }    
+
+
+@Repository
+public interface CardOnboardingCacheRepository extends CrudRepository<CardOnboardingCache, Long> {
+
+    List<PdxInstance> findBymId(String mId);
+
+}
+
+public static <T> List<T> mapPdxList(List<PdxInstance> pdxInstances) {
+        return (List<T>) pdxInstances.stream().map(PdxInstance::getObject).toList();
     }
+
+this code will work
+ 
+
+same like this i want for single object non list
+
+
+    public Optional<MerchantNotificationCache> fetchNotificationViewByMid(String mId) {
+        try {
+            Optional<MerchantNotificationCache> merchantCacheDetails =  merchantNotificationCacheRepository.findById(mId);
+            return Optional.ofNullable((MerchantNotificationCache) ((PdxInstance) merchantCacheDetails.get()).getObject());
+        } catch (Exception e) {
+            logger.error("GemFire cache lookup failed or GemFire server down. Reason: {}", e.getMessage());
+        }
+        return Optional.empty();
+    }
+
+
+
+@NoRepositoryBean
+public interface CrudRepository<T, ID> extends Repository<T, ID> {
+    <S extends T> S save(S entity);
+
+    <S extends T> Iterable<S> saveAll(Iterable<S> entities);
+
+    Optional<T> findById(ID id);
+
+    boolean existsById(ID id);
+
+    Iterable<T> findAll();
+
+    Iterable<T> findAllById(Iterable<ID> ids);
+
+    long count();
+
+    void deleteById(ID id);
+
+    void delete(T entity);
+
+    void deleteAllById(Iterable<? extends ID> ids);
+
+    void deleteAll(Iterable<? extends T> entities);
+
+    void deleteAll();
+}
+
