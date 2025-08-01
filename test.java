9cfef1302175e5fa1c638160795ec05d809d3fb6
lohithmp <109272620+lohithmp@users.noreply.github.com>
2025-08-01 10:36:12 +0530

    Update test.java

--- test.java
+++ test.java
@@ -6,90 +6,178 @@ import com.sbi.epay.notification.exception.NotificationException;
 import com.sbi.epay.notification.model.EmailDto;
 import com.sbi.epay.notification.service.EmailTemplateService;
 import com.sbi.epay.notification.util.NotificationConstant;
+import io.micrometer.common.util.StringUtils;
+import jakarta.mail.*;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeBodyPart;
+import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
-import javax.mail.MessagingException;
-import javax.mail.internet.MimeMessage;
 import java.text.MessageFormat;
-
-/**
- * Class Name: EmailClient
- * *
- * Description:EmailClient this class mainly used for sending mail and createMessage and MimeMessageHelper
- * *
- * Author: V1017903 (bhushan wadekar)
- * Copyright (c) 2024 [State Bank of India]
- * All rights reserved
- * *
- * Version:1.0
- */
+import java.util.Arrays;
+import java.util.Properties;
 
 @Service
 @RequiredArgsConstructor
-public class EmailClient {
-    private final JavaMailSender javaMailSender;
-    private final EmailTemplateService emailTemplateService;
-    LoggerUtility logger = LoggerFactoryUtility.getLogger(EmailClient.class);
+public class EmailClientService {
 
-    /**
-     * This method will be used for calling createMimeMessage and send method
-     *
-     * @param emailDTO object for calling createMimeMessage method
-     * @return true if email send successfully otherwise return false
-     * @throws NotificationException if any exception occur
-     */
-    public boolean sendEmail(EmailDto emailDTO) throws NotificationException {
+    private final EmailTemplateService emailTemplateService;
+
+    LoggerUtility logger = LoggerFactoryUtility.getLogger(EmailClientService.class);
+
+    public Session createMailSession() {
+        Properties props = new Properties();
+
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.host", "XXXXXXX");
+        props.put("mail.smtp.port", "587");
+
+
+        return Session.getInstance(props, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication("XXXX", "XXXXX");
+            }
+        });
+    }
+
+    public boolean sendEmail(EmailDto emailDto) {
         logger.info("ClassName - EmailClient,MethodName - sendEmail,Method-start");
+
         try {
-            MimeMessage message = createMimeMessage(emailDTO);
-            javaMailSender.send(message);
+            Message message = createMimeMessage(emailDto);
+            Transport.send(message);
             return true;
+        } catch (SendFailedException sfe) {
+            logger.error("ClassName - EmailClient,MethodName - sendEmail, inside catch, SendFailedException"+ Arrays.toString(sfe.getInvalidAddresses()) + sfe);
+            throw new NotificationException(NotificationConstant.FAILURE_CODE, MessageFormat.format(NotificationConstant.FAILURE_MSG, "Email"));
         } catch (MessagingException e) {
-            logger.info("ClassName - EmailClient,MethodName - sendEmail, inside catch" + e);
-            throw new NotificationException(NotificationConstant.FAILURE_CODE, MessageFormat.format(NotificationConstant.FAILURE_MSG, "EMail"));
+            logger.error("ClassName - EmailClient,MethodName - sendEmail, inside catch" + e);
+            throw new NotificationException(NotificationConstant.FAILURE_CODE, MessageFormat.format(NotificationConstant.FAILURE_MSG, "Email"));
         }
     }
 
-    /**
-     * This method will be used for createMimeMessage
-     *
-     * @param emailDTO object for mimeMessage
-     * @return notification message
-     * @throws MessagingException if any exception occur
-     */
-    private MimeMessage createMimeMessage(EmailDto emailDTO) throws MessagingException {
+    private Message createMimeMessage(EmailDto emailDto) throws MessagingException {
         logger.info("ClassName - EmailClient,MethodName - createMimeMessage,Method-start");
-        MimeMessage message = javaMailSender.createMimeMessage();
-        setMimeMessageHelper(emailDTO, message);
+        Session session = createMailSession();
+        Message message = new MimeMessage(session);
+        setMimeMessageHelper(emailDto, message);
         logger.info("ClassName - EmailClient,MethodName - createMimeMessage,Method-end");
         return message;
     }
 
-    /**
-     * This method will be used for set email details into MimeMessageHelper
-     *
-     * @param emailDTO for set parameter into MimeMessageHelper
-     * @param message MimeMessage
-     */
-    private void setMimeMessageHelper(EmailDto emailDTO, MimeMessage message) throws MessagingException {
+    private void setMimeMessageHelper(EmailDto emailDto, Message message) throws MessagingException {
         logger.info("ClassName - EmailClient,MethodName - setMimeMessageHelper,Method-start");
-        MimeMessageHelper helper = new MimeMessageHelper(message, true);
-        helper.setFrom(emailDTO.getFrom());
-        helper.setTo(emailDTO.getRecipient());
-        helper.setSubject(emailDTO.getSubject());
-        String emailContent = emailTemplateService.generateEmailBody(emailDTO.getEmailTemplate(), emailDTO.getBody());
-        helper.setText(emailContent, true); // true indicates HTML content
-        if (StringUtils.isNotEmpty(emailDTO.getCc())) {
-            helper.setCc(emailDTO.getCc());
+        message.setFrom(new InternetAddress(emailDto.getFrom()));
+
+        message.setRecipients(MimeMessage.RecipientType.TO, InternetAddress.parse(emailDto.getRecipient()));
+        message.setSubject(emailDto.getSubject());
+
+        String emailContent = emailTemplateService.generateEmailBody(emailDto.getEmailTemplate(), emailDto.getBody());
+        MimeBodyPart htmlPart = new MimeBodyPart();
+        htmlPart.setContent(emailContent, "text/html");
+
+        if (StringUtils.isNotEmpty(emailDto.getCc())) {
+            message.setRecipients(Message.RecipientType.CC, InternetAddress.parse(emailDto.getCc()));
         }
-        if (StringUtils.isNotEmpty(emailDTO.getBcc())) {
-            helper.setBcc(emailDTO.getBcc());
+        if (StringUtils.isNotEmpty(emailDto.getBcc())) {
+            message.setRecipients(Message.RecipientType.BCC, InternetAddress.parse(emailDto.getBcc()));
         }
         logger.info("ClassName - EmailClient,MethodName - setMimeMessageHelper,Method-end");
     }
+}
 
+
+
+plugins {
+	id 'java'
+	id 'maven-publish'
+}
+
+group = 'com.sbi.epay'
+version = "${version}"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+configurations {
+	compileOnly {
+		extendsFrom annotationProcessor
+	}
+}
+
+repositories {
+	mavenCentral()
+	maven {
+		url "https://gitlab.epay.sbi/api/v4/projects/16/packages/maven"
+		credentials(PasswordCredentials) {
+			username = project.findProperty("gitlab.username") ?: System.getenv("CI_USERNAME")
+			password = project.findProperty("gitlab.token") ?: System.getenv("CI_JOB_TOKEN")
+		}
+		authentication {
+			basic(BasicAuthentication)
+		}
+	}
+}
+
+publishing {
+	publications {
+		mavenJava(MavenPublication) {
+			from components.java
+		}
+	}
+	repositories {
+		maven {
+			url "https://gitlab.epay.sbi/api/v4/projects/16/packages/maven"
+			credentials(PasswordCredentials) {
+				username = project.findProperty("gitlab.username") ?: System.getenv("CI_USERNAME")
+				password = project.findProperty("gitlab.token") ?: System.getenv("CI_JOB_TOKEN")
+			}
+			authentication {
+				basic(BasicAuthentication)
+			}
+		}
+	}
+}
+
+dependencies {
+	implementation "org.springframework:spring-webflux:${webflux}"
+	implementation "org.springframework:spring-webmvc:${webmvc}"
+	implementation "org.springframework:spring-context-support:${spring_context}"
+
+	implementation "org.thymeleaf:thymeleaf:${thymeleaf}"
+	implementation "org.thymeleaf:thymeleaf-spring5:${thymeleaf}"
+	implementation "org.hibernate.validator:hibernate-validator:${hibernate}"
+
+	implementation "com.sun.mail:jakarta.mail:2.0.1"
+	implementation "org.eclipse.angus:angus-mail:2.0.2"
+
+
+//	implementation "com.google.code.gson:gson:${gson}"
+	implementation "org.apache.commons:commons-lang3:${apache_common}"
+
+	// Lombok
+	compileOnly "org.projectlombok:lombok:${lombok}"
+	annotationProcessor "org.projectlombok:lombok:${lombok}"
+
+	implementation "com.sbi.epay:logging-service:${logg_version}"
+//	implementation "javax.servlet:javax.servlet-api:${javax_servlet}"
+
+	// Mockito
+	testImplementation "org.mockito:mockito-core:${mockito}"
+	// JUnit Platform Launcher
+	testRuntimeOnly "org.junit.platform:junit-platform-launcher:${junit}"
+	// JUnit Jupiter (for running JUnit 5 tests)
+	testImplementation "org.junit.jupiter:junit-jupiter:${junit_jupiter}"
+	testRuntimeOnly 'org.junit.platform:junit-platform-launcher'
+}
+
+tasks.named('test') {
+	useJUnitPlatform()
 }
