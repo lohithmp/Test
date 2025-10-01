a061c224a443107f4d3858a75358d4748f496bca
lohithmp <109272620+lohithmp@users.noreply.github.com>
2025-10-01 15:22:30 +0530

    Update test.java

--- test.java
+++ test.java
@@ -1,13 +1,27 @@
+ public Claims verifyJwtTokenAndGetClaims(String token) {
+        Claims claims = getAllClaimsFromToken(token);
+        if (issuer.equalsIgnoreCase(claims.getIssuer())) {
+            if (isTokenExpired(claims.getExpiration())) {
+                throw new IllegalStateException(EPayAuthenticationErrorConstants.TOKEN_EXPIRED);
+            } else if (authenticationUserService.isTokenInValid(token, claims.get(TYPE, String.class))) {
+                throw new IllegalStateException(EPayAuthenticationErrorConstants.INVALID_TOKEN);
+            }
+            String userName = (String) claims.get(USERNAME);
+            String userNameWithTokenType = String.join(EPayAuthenticationConstant.JOINER, (String) claims.get(USERNAME), claims.get(TYPE, String.class));
+            logger.info("Token request received for userNameWithTokenType: {}", userNameWithTokenType);
+            EPayPrincipal ePayPrincipal = authenticationUserService.loadUserByUserName(userNameWithTokenType).orElseThrow(() -> new EPaySecurityException(EPayAuthenticationErrorConstants.NOT_FOUND_ERROR_CODE, MessageFormat.format(EPayAuthenticationErrorConstants.NOT_FOUND_ERROR_MESSAGE, EPayAuthenticationErrorConstants.USER_NAME)));
+            logger.info("Token request received for userNameWithTokenType: {}", userNameWithTokenType);
+            if (!ePayPrincipal.getAuthenticationId().equalsIgnoreCase(userName)) {
+                throw new IllegalStateException(EPayAuthenticationErrorConstants.INVALID_USER);
+            }
+        } else {
+            throw new IllegalStateException(EPayAuthenticationErrorConstants.INVALID_ISSUER);
+        }
+        return claims;
+    }
 
-security:
-  jwt:
-    secret:
-      key: gdjfgskjfhsdjkhkflkdlksdlfkskfwperip3ke3le3lmldrnkfnhiewjfejfokepfkldkfoikfokork3dklwedlsvflvkfkvlkdfvodkvcdokro3
+public interface AuthenticationUserService {
+    Optional<EPayPrincipal> loadUserByUserName(String userName);
 
-    implementation "io.jsonwebtoken:jjwt-api:${jwt}"
-    runtimeOnly "io.jsonwebtoken:jjwt-impl:${jwt}"
-    runtimeOnly "io.jsonwebtoken:jjwt-jackson:${jwt}"
-
-
-        
-              
+    boolean isTokenInValid(String token, String tokenType);
+}
