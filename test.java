35fc7d49839aff16d562cbce337a8456b6840932
lohithmp <109272620+lohithmp@users.noreply.github.com>
2025-10-31 12:28:18 +0530

    Update test.java

--- test.java
+++ test.java
@@ -7,3 +7,15 @@
     <display-name>Archetype Created Web Application</display-name>
 
 </web-app>
+
+
+
+         <web-app xmlns="http://xmlns.jcp.org/xml/ns/javaee"
+         xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+         xsi:schemaLocation="http://xmlns.jcp.org/xml/ns/javaee
+                             http://xmlns.jcp.org/xml/ns/javaee/web-app_3_0.xsd"
+         version="3.0">
+
+    <display-name>My JSP Servlet App</display-name>
+
+</web-app>
