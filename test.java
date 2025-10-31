ff1e99b7051079cc054f7883dcf0f6db1fdfbcd1
lohithmp <109272620+lohithmp@users.noreply.github.com>
2025-10-31 12:49:23 +0530

    Update test.java

--- test.java
+++ test.java
@@ -1,6 +1,1 @@
-<dependency>
-    <groupId>javax.servlet</groupId>
-    <artifactId>javax.servlet-api</artifactId>
-    <version>3.1.0</version>
-    <scope>provided</scope>
-</dependency>
+Could not transfer artifact javax.servlet:javax.servlet-api:pom:3.1.0 from/to central (https://repo.maven.apache.org/maven2): No such host is known (repo.maven.apache.org)
