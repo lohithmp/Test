cb0654d549a86d016c7b184a046970b152384996
lohithmp <109272620+lohithmp@users.noreply.github.com>
2025-10-31 12:10:32 +0530

    Update test.java

--- test.java
+++ test.java
@@ -1,27 +1,7 @@
- public Claims verifyJwtTokenAndGetClaims(String token) {
-        Claims claims = getAllClaimsFromToken(token);
-        if (issuer.equalsIgnoreCase(claims.getIssuer())) {
-            if (isTokenExpired(claims.getExpiration())) {
-                throw new IllegalStateException(EPayAuthenticationErrorConstants.TOKEN_EXPIRED);
-            } else if (authenticationUserService.isTokenInValid(token, claims.get(TYPE, String.class))) {
-                throw new IllegalStateException(EPayAuthenticationErrorConstants.INVALID_TOKEN);
-            }
-            String userName = (String) claims.get(USERNAME);
-            String userNameWithTokenType = String.join(EPayAuthenticationConstant.JOINER, (String) claims.get(USERNAME), claims.get(TYPE, String.class));
-            logger.info("Token request received for userNameWithTokenType: {}", userNameWithTokenType);
-            EPayPrincipal ePayPrincipal = authenticationUserService.loadUserByUserName(userNameWithTokenType).orElseThrow(() -> new EPaySecurityException(EPayAuthenticationErrorConstants.NOT_FOUND_ERROR_CODE, MessageFormat.format(EPayAuthenticationErrorConstants.NOT_FOUND_ERROR_MESSAGE, EPayAuthenticationErrorConstants.USER_NAME)));
-            logger.info("Token request received for userNameWithTokenType: {}", userNameWithTokenType);
-            if (!ePayPrincipal.getAuthenticationId().equalsIgnoreCase(userName)) {
-                throw new IllegalStateException(EPayAuthenticationErrorConstants.INVALID_USER);
-            }
-        } else {
-            throw new IllegalStateException(EPayAuthenticationErrorConstants.INVALID_ISSUER);
-        }
-        return claims;
-    }
+<!DOCTYPE web-app PUBLIC
+ "-//Sun Microsystems, Inc.//DTD Web Application 3//EN"
+ "http://java.sun.com/dtd/web-app_3.dtd" >
 
-public interface AuthenticationUserService {
-    Optional<EPayPrincipal> loadUserByUserName(String userName);
-
-    boolean isTokenInValid(String token, String tokenType);
-}
+<web-app>
+  <display-name>Archetype Created Web Application</display-name>
+</web-app>
