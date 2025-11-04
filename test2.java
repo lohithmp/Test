bdb416be16b74bfc81d5fe21a0a7af95248a4732
lohithmp <109272620+lohithmp@users.noreply.github.com>
2025-11-04 20:35:15 +0530

    Update test2.java

--- test2.java
+++ test2.java
@@ -1,58 +1,38 @@
- String txnResponse = "{\"status\": 1, \"data\": [{\"merchantReferenceNumber\": \"268280741\", \"sbiEpayReferenceNumber\": \"1F9FF78F3A644EE79CFF\", \"bankReferenceNumber\": \"268280741\", \"transactionDateAndTime\": 1757400860154, \"transactionAmount\": 1, \"totalAmount\": 2.18, \"transactionStatus\": \"SUCCESS\", \"transactionStatusDescription\": \"SUCCESS\", \"settlementDate\": null, \"cinNumber\": \"10000030909202563108\", \"payMode\": \"CC\", \"channelBank\": \"OTHERS\", \"cardType\": \"MASTER\", \"refundDetails\": [{\"refundReferenceNumber\": \"DC003174590772767481\", \"refundBookingDateAndTime\": 1745907729125, \"refundType\": \"PARTIAL\", \"refundAmount\": 0.01, \"refundStatus\": \"REFUND_PROCESSED\", \"refundProcessedDateAndTime\": 1757412480523}], \"count\": 1, \"total\": 1}]}";
+<%@ page import="java.util.*, com.fasterxml.jackson.databind.*, com.fasterxml.jackson.core.type.TypeReference" %>
+
+<%
+try {
+    String txnResponse = "{\"status\": 1, \"data\": [{\"merchantReferenceNumber\": \"268280741\", \"sbiEpayReferenceNumber\": \"1F9FF78F3A644EE79CFF\", \"bankReferenceNumber\": \"268280741\", \"transactionDateAndTime\": 1757400860154, \"transactionAmount\": 1, \"totalAmount\": 2.18, \"transactionStatus\": \"SUCCESS\", \"transactionStatusDescription\": \"SUCCESS\", \"settlementDate\": null, \"cinNumber\": \"10000030909202563108\", \"payMode\": \"CC\", \"channelBank\": \"OTHERS\", \"cardType\": \"MASTER\", \"refundDetails\": [{\"refundReferenceNumber\": \"DC003174590772767481\", \"refundBookingDateAndTime\": 1745907729125, \"refundType\": \"PARTIAL\", \"refundAmount\": 0.01, \"refundStatus\": \"REFUND_PROCESSED\", \"refundProcessedDateAndTime\": 1757412480523}], \"count\": 1, \"total\": 1}]}";
 
     ObjectMapper mapper = new ObjectMapper();
 
     Map<String, Object> txnDetails1 = mapper.readValue(
         txnResponse,
-        new TypeReference<Map<String, Object>>(){}
+        new TypeReference<Map<String, Object>>() {}
     );
 
     ArrayList dataList = (ArrayList) txnDetails1.get("data");
-
-    if (!dataList.isEmpty()) {
-    }
-
     ArrayList aggRefundResultList = new ArrayList();
 
+    if (dataList != null && !dataList.isEmpty()) {
+        for (int i = 0; i < dataList.size(); i++) {
+            Map<String, Object> transaction = (Map<String, Object>) dataList.get(i);
 
-	ArrayList transTrackGetList = new ArrayList();
-	ArrayList transTrackDetailList = new ArrayList();
-	
-    int size = 0;
-    int listSize = 0;
-    int flag = 0;
+            out.println("<pre>Transaction: " + transaction + "</pre>");
 
-    	try	{
-        		transTrackGetList = dataList;
-        		listSize = transTrackGetList.size();
+            if (transaction.containsKey("refundDetails")) {
+                ArrayList<Map<String, Object>> refundDetails = (ArrayList<Map<String, Object>>) transaction.get("refundDetails");
 
-        		if(listSize>0) {
-        			flag = 1;
-        		    for(int i=0; listSize>i; i++) {
-                        transTrackDetailList = (ArrayList) transTrackGetList.get(i);
-        		    }
-        	    }
-
-                out.println("<pre>transTrackDetailList: "+transTrackDetailList+"</pre>");
-            }
-        catch(Exception t)  {
-        }
-
-
-        if(listSize>0) {
-         	for(int i=0; i<transTrackDetailList.size(); i++) {
-         	    if(transTrackDetailList.get("refundDetails") && transTrackDetailList.get("refundDetails").get(i) && transTrackDetailList.get("refundDetails").get(i).size()>0) {
-                    aggRefundResultList = transTrackDetailList.refundDetails.get(i);
-                } else {
-                    aggRefundResultList = 0;
+                if (refundDetails != null && !refundDetails.isEmpty()) {
+                    aggRefundResultList.addAll(refundDetails);
                 }
-        	}
-        } else {
-        	flag = 0;
+            }
         }
+    }
 
+    out.println("<pre>aggRefundResultList: " + aggRefundResultList + "</pre>");
 
-
-
-The method get(int) in the type ArrayList is not applicable for the arguments (String)
-
+} catch (Exception e) {
+    out.println("<pre style='color:red;'>Error: " + e + "</pre>");
+}
+%>
