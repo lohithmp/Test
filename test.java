89f4ac6794772c9cbf3c3a591c8226c7711c0430
lohithmp <109272620+lohithmp@users.noreply.github.com>
2025-12-06 19:08:11 +0530

    Update test.java

--- test.java
+++ test.java
@@ -1,9 +1,32 @@
-java.lang.IllegalArgumentException: Cannot construct instance of `com.epay.transaction.model.request.PaymentInitiationRequest` (although at least one Creator exists): no String-argument constructor/factory method to deserialize from String value ('{"operatingMode": "someMode", "payProcId": "proc123", "payProcType": "someType", "gtwMapsId": "gtw456", "pgBankCode": "HDFC", "merchPostedAmount": 100.00, "transactionAmount": 100.00, "upiAddress": "user@vpa", "channelBank": "HDFC", "altNumber": "111", "expiryMonth": "111", "expiryYear": "2026", "cvv": "877", "cardHolderName": "ganesh"}')
- at [Source: UNKNOWN; byte offset: #UNKNOWN]
+    public TransactionResponse<EncryptedResponse> initiatePayment() throws JsonProcessingException {
+        logger.info("Payment Initiation processing request");
+        EPayPrincipal ePayPrincipal = EPayIdentityUtil.getUserPrincipal();
+        PaymentBookingDto paymentInitiationDto = paymentBookingDao.getPaymentBooking(ePayPrincipal.getOrderRef());
+        String mek = paymentBookingDao.getMerchantMek();
+        PaymentInitiationRequest paymentInitiationRequest = mapper.readValue(paymentInitiationDto.getPaymentObject(), PaymentInitiationRequest.class);
+        OrderDto orderDto = mapper.readValue(paymentInitiationDto.getOrderObject(), OrderDto.class);
+        EncryptedResponse encryptedResponse = paymentInitiationDao.processRequest(paymentInitiationRequest, paymentInitiationDto.getPayMode(), orderDto, mek);
+        return TransactionResponse.<EncryptedResponse>builder().data(List.of(encryptedResponse)).status(TransactionConstant.RESPONSE_SUCCESS).build();
+    }
 
-    PaymentInitiationRequest paymentInitiationRequest = mapper.convertValue(paymentInitiationDto.getPaymentObject(), PaymentInitiationRequest.class);
 
 @Data
+@RequiredArgsConstructor
+public class PaymentBookingDto {
+    private UUID id;
+    private String mId;
+    private String sbiOrderRefNumber;
+    private String orderHash;
+    private String orderObject;
+    private String paymentObject;
+    private Long expiryTime;
+    private String payMode;
+}
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class PaymentInitiationRequest {
 
     private String operatingMode;
@@ -37,15 +60,11 @@ public class PaymentInitiationRequest {
 
 }
 
-@Data
-@RequiredArgsConstructor
-public class PaymentBookingDto {
-    private UUID id;
-    private String mId;
-    private String sbiOrderRefNumber;
-    private String orderHash;
-    private String orderObject;
-    private String paymentObject;
-    private Long expiryTime;
-    private String payMode;
-}
+
+
+in oracle database i inserted to the payment_object column as this object like this and type is CLOB
+ '{"operatingMode": "someMode", "payProcId": "proc123", "payProcType": "someType", "gtwMapsId": "gtw456", "pgBankCode": "HDFC", "merchPostedAmount": 100.00, "transactionAmount": 100.00, "upiAddress": "user@vpa", "channelBank": "HDFC", "altNumber": "111", "expiryMonth": "111", "expiryYear": "2026", "cvv": "877", "cardHolderName": "ganesh"}'
+
+ correct me why im getting this errror
+
+ Unexpected character (''' (code 39)): expected a valid value (JSON String, Number, Array, Object or token 'null', 'true' or 'false')
